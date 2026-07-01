fa4cb62e054859efd6f67d564f1caf33fd464376
pratik376 <babariyapratik208@gmail.com>
2026-06-30 19:53:37 -0700

    Your commit message

--- Trees/New folder/863. All Nodes Distance K in Binary Tree.py
+++ Trees/New folder/863. All Nodes Distance K in Binary Tree.py
@@ -1,6 +1,6 @@
 # Definition for a binary tree node.
 from typing import List
-
+from collections import deque
 # find all nodes exactly k steps away from target
 
 # is a big hint for BFS.
@@ -14,6 +14,51 @@ class TreeNode:
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        pass
+
+        parent={}
+
+        def find_parents(node, parent):
+
+            if not node:
+                return
+            
+            parent[node]=parent
+
+            find_parents(node.left)
+            find_parents(node.right)
+        find_parents(root,None)
+
+        queue=deque()
+        queue.append((target,0))
+
+        visted= set()
+        visted.add(target)
+
+        answer=[]
+
+        while queue:
+
+            node,distance=queue.popleft()
+
+            if distance==k:
+                answer.append(node.val)
+            
+            if distance>k:
+                break
+
+            neighbours= [node.left, node.right, parent[node]]
+
+            for nei in neighbours:
+
+                if nei and nei not in visted:
+                    visted.add(nei)
+                    queue.append((nei, distance+1))
+        
+        return answer
+
+
+        
+        
+
 
         
