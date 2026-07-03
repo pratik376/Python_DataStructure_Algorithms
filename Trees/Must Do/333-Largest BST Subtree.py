ca72a85be8db2086179496a0e2af24ac0b9b411d
pratik376 <babariyapratik208@gmail.com>
2026-07-02 19:08:56 -0700

    Add new DSA practice files

--- Trees/Must Do/333-Largest BST Subtree.py
+++ Trees/Must Do/333-Largest BST Subtree.py
@@ -1,13 +1,31 @@
 from typing import Optional
+import math
 
-
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
-    def LargestBST(self, root: Optional[TreeNode], target:float, k:int) -> list[int]:
+    def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
 
+        def dfs(node):
+            if not node:
+                # is_bst, size, min_val, max_val
+                return True, 0, math.inf, -math.inf
 
-       
+            left_is_bst, left_size, left_min, left_max = dfs(node.left)
+            right_is_bst, right_size, right_min, right_max = dfs(node.right)
+
+            if left_is_bst and right_is_bst and left_max < node.val < right_min:
+                size = left_size + right_size + 1
+                self.ans = max(self.ans, size)
+                return True, size, min(left_min, node.val), max(right_max, node.val)
+
+            return False, 0, 0, 0
+
+        dfs(root)
+        return self.ans
