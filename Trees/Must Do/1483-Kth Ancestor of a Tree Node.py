728e4de1921c5e73d5d3e76093fb088c49fed3dd
pratik376 <babariyapratik208@gmail.com>
2026-07-04 16:22:46 -0700

    Add new DSA practice files

---
+++ Trees/Must Do/1483-Kth Ancestor of a Tree Node.py
@@ -0,0 +1,48 @@
+from typing import Optional
+import math
+from collections import defaultdict
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def KthAncestor(self, root: Optional[TreeNode], nodes: Optional[TreeNode]) -> int:
+
+        target_nodes= set(nodes)
+        self.answer= None
+        target_count=len(target_nodes)
+
+
+        def dfs(node):
+
+            if not node:
+                return 0
+            
+            left_sum=dfs(node.left)
+            right_sum=dfs(node.right)
+
+            curr_sum= left_sum + right_sum + (1 if node in target_nodes else 0)
+
+            if curr_sum == target_count and self.answer is None:
+                self.answer=node
+            
+            return curr_sum
+        dfs(root)
+        return self.answer
+       
+
+
+        
+
+
+
+            
+
+
+
+
+
+        return self.ans
