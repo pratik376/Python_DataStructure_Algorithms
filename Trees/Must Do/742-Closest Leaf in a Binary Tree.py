88f0c00d7fdbe6130c34585ab71669aba7017b31
pratik376 <babariyapratik208@gmail.com>
2026-07-04 17:56:38 -0700

    Add new DSA practice files

--- Trees/Must Do/742-Closest Leaf in a Binary Tree.py
+++ Trees/Must Do/742-Closest Leaf in a Binary Tree.py
@@ -11,19 +11,52 @@ class Solution:
     def findClosestLeaft(self, root: Optional[TreeNode],k : int) -> int:
         
         parent= {}
+        target_node=None
 
         def find_parents(node):
 
             if not node:
                 return
-            
+            if node.val == k:
+                target_node=node
+
             if node.left:
                 parent[node.left]=node
             if node.right:
                 parent[node.right]=node
+            
+            find_parents(node.left)
+            find_parents(node.right)
         find_parents(root)
 
-        q=deque(())
+        q=deque(target_node)
+        visited= set(target_node)
+        answer=0
+
+        while q:
+
+            curr= q.popleft()
+
+            if not curr.left and not curr.right:
+                return curr.val
+
+            if curr.left and curr.left not in visited:
+                q.append(curr.left)
+                visited.add(curr.left)
+            
+            if curr.right and curr.right not in visited:
+                q.append(curr.right)
+                visited.add(curr.right)
+            
+            if parent[curr] and parent[curr] not in visited:
+                q.append(parent[curr])
+                visited.add(parent[curr])
+        
+        return -1
+
+
+
+
 
 
             
