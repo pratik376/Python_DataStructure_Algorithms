cd864e5fc5559d4b830209117ae2a380b1a128f8
pratik376 <babariyapratik208@gmail.com>
2026-07-05 17:58:10 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/1. Heap Basics/215. Kth Largest Element in an Array.py
+++ Heap_Priority_Queue/1. Heap Basics/215. Kth Largest Element in an Array.py
@@ -1,3 +1,15 @@
+from typing import List
+import heapq
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        nums= [ -x for x in nums]
+
+        heapq.heapify(nums)
+
+        while k>0:
+            element= heapq.heappop(nums)
+            k-=1
+        return -element
         
