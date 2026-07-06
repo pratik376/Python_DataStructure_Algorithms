6f5ab3ae7c1911a6140405c18831f5155d3d7b98
pratik376 <babariyapratik208@gmail.com>
2026-07-05 17:54:25 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/1. Heap Basics/LC 1046 — Last Stone Weight.py
+++ Heap_Priority_Queue/1. Heap Basics/LC 1046 — Last Stone Weight.py
@@ -20,6 +20,23 @@ class Solution:
         
         return heap[0] if heap else 0
         
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+       
+        stones=[-val for val in stones]
+
+        heapq.heapify(stones) # it takes one list na donly one argument
+        
+        while len(stones)>=2:
+            x= -heapq.heappop(stones)
+            y= -heapq.heappop(stones)
+
+            if x != y:
+                heapq.heappush_max(stones,-(x-y))
+        
+        return -stones[0] if stones else 0
+        
 
         
         
