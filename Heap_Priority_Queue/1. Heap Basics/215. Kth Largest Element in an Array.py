cee63b2b0aee81bf09a2d16f7614922d0c10462d
pratik376 <babariyapratik208@gmail.com>
2026-07-05 19:03:17 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/1. Heap Basics/215. Kth Largest Element in an Array.py
+++ Heap_Priority_Queue/1. Heap Basics/215. Kth Largest Element in an Array.py
@@ -14,3 +14,19 @@ class Solution:
         return -element
     
     # total time O(n + k * logn ) space is O(n)
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+       heap=[]
+
+       for num in nums:
+           heapq.heappush(heap,num)
+
+           if len(heap)> k:
+               heapq.heappop(heap)
+       return heap[0]
+        
+       
+    
+    # total time O(n + k * logn ) space is O(n)
