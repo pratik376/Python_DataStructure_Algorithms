38e22a96d950c167c235de8c8a84cf91ee790e9b
pratik376 <babariyapratik208@gmail.com>
2026-07-07 16:47:37 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/1. Heap Basics/703. Kth Largest Element in a Stream.py
+++ Heap_Priority_Queue/1. Heap Basics/703. Kth Largest Element in a Stream.py
@@ -1,9 +1,22 @@
+from typing import List
+import heapq
+
+
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
+        self.k=k
+        self.nums=nums
+
+        self.nums=[-val for val in self.nums]
+        heapq.heapify(self.nums)
         
 
     def add(self, val: int) -> int:
+        heapq.heappush(self.nums,-val)
+
+        return -self.nums[self.k-1]
+        
         
 
 
