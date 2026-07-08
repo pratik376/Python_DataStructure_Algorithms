b231b0db362aa924231404207d1e83d495dc8fa2
pratik376 <babariyapratik208@gmail.com>
2026-07-07 17:51:27 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/1. Heap Basics/LC 912 — Sort an Array using HeapSort.py
+++ Heap_Priority_Queue/1. Heap Basics/LC 912 — Sort an Array using HeapSort.py
@@ -12,3 +12,19 @@
         
 #         return answer
 
+
+import heapq
+
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+
+        nums= [-val for val in nums]
+
+        heapq.heapify(nums)
+
+        for i in range(len(nums)-1, -1,-1):
+
+            largest=-heapq.heappop(nums)
+            nums[i]=largest
+        
+        return nums
