053cb18ccf2e0b2e35fa49120feaaee535c2961a
pratik376 <babariyapratik208@gmail.com>
2026-07-07 17:33:27 -0700

    Add new DSA practice files

---
+++ Heap_Priority_Queue/1. Heap Basics/LC 912 — Sort an Array using HeapSort.py
@@ -0,0 +1,14 @@
+import heapq
+
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+
+        answer=[]
+
+        heapq.heapify(nums)
+
+        while len(nums)> 0:
+            answer.append(heapq.heappop(nums))
+        
+        return answer
+
