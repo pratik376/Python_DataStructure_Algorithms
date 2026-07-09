2dc8f0ebe02cf9b76c73e11cb02a220a0e7a7c58
pratik376 <babariyapratik208@gmail.com>
2026-07-09 16:12:04 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 1086 — High Five.py
+++ Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 1086 — High Five.py
@@ -26,7 +26,26 @@ class Solution:
 
     return answer
 
-    
+from typing import List
+import heapq
+from collections import defaultdict
+
+class Solution:
+    def highFive(self, items: List[List[int]]) -> List[List[int]]:
+        score_dict = defaultdict(list)
+
+        for student_id, score in items:
+            heapq.heappush(score_dict[student_id], score)
+            if len(score_dict[student_id]) > 5:
+                heapq.heappop(score_dict[student_id])
+
+        answer = []
+
+        for student_id in sorted(score_dict.keys()):
+            avg = sum(score_dict[student_id]) // 5
+            answer.append([student_id, avg])
+
+        return answer
      
 
 
