ab5fee252fcb8fddd337009e37db3078343305b2
pratik376 <babariyapratik208@gmail.com>
2026-07-08 19:43:34 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/1. Heap Basics/Convert Min Heap to Max Heap — GFG.py
+++ Heap_Priority_Queue/1. Heap Basics/Convert Min Heap to Max Heap — GFG.py
@@ -1,14 +1,64 @@
-from typing import List
-import heapq
+# To heapify a subtree 
+def heapify(arr, n, i):
+    
+    # Initialize largest as root
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
 
-class Solution:
-    def minHeapToMaxHeap(self, heap: List[int]) -> List[int]:
+    # If left child is larger than root
+    if l < n and arr[l] > arr[largest]:
+        largest = l
 
-        heap = [-val for val in heap]
-        heapq.heapify(heap)
+    # If right child is larger than largest so far
+    if r < n and arr[r] > arr[largest]:
+        largest = r
 
-        heap = [-val for val in heap]
+    # If largest is not root
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
 
-        return heap
+        # Recursively heapify the affected sub-tree
+        heapify(arr, n, largest)
 
+# Function to build a Max-Heap from the given array
+def buildHeap(arr):
+    n = len(arr)
+    
+    # Index of last non-leaf node
+    startIdx = (n // 2) - 1
 
+    # Perform reverse level order traversal
+    # from last non-leaf node and heapify
+    # each node
+    for i in range(startIdx, -1, -1):
+        heapify(arr, n, i)
+
+if __name__ == "__main__":
+    # Binary Tree Representation
+    # of input array
+    #             1
+    #           /    \
+    #         3        5
+    #       /  \     /  \
+    #     4      6  13  10
+    #    / \    / \
+    #   9   8  15 17
+    arr = [1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17]
+    n = len(arr)
+
+    # Function call
+    buildHeap(arr)
+
+    for i in range(n):
+        print(arr[i], end=" ")
+    print()
+
+    # Final Heap:
+    #              17
+    #            /    \
+    #          15      13
+    #         /  \     / \
+    #        9     6  5   10
+    #       / \   / \
+    #      4   8 3   1
