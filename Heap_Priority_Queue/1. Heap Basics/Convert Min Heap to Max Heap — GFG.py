bf80f7fbc1825b27f0a0aeea79f2b8d579daed7c
pratik376 <babariyapratik208@gmail.com>
2026-07-08 19:43:42 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/1. Heap Basics/Convert Min Heap to Max Heap — GFG.py
+++ Heap_Priority_Queue/1. Heap Basics/Convert Min Heap to Max Heap — GFG.py
@@ -1,3 +1,17 @@
+from typing import List
+import heapq
+
+class Solution:
+    def minHeapToMaxHeap(self, heap: List[int]) -> List[int]:
+
+        heap = [-val for val in heap]
+        heapq.heapify(heap)
+
+        heap = [-val for val in heap]
+
+        return heap
+
+
 # To heapify a subtree 
 def heapify(arr, n, i):
     
