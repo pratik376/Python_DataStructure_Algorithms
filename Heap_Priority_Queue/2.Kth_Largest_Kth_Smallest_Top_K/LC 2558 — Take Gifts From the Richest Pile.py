5efd6530d6be6d95a9a19262df3a2165a46f3be7
pratik376 <babariyapratik208@gmail.com>
2026-07-09 15:37:26 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 2558 — Take Gifts From the Richest Pile.py
+++ Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 2558 — Take Gifts From the Richest Pile.py
@@ -1,6 +1,35 @@
 from typing import List
-
+import heapq
+import math
 
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+
+        
+
+       gifts=[ -val for val in gifts]
+
+       heapq.heapify(gifts)
+
+       while k >0:
+          largets= -heapq.heappop(gifts)
+
+          cal_val=-math.floor(math.sqrt(largets))
+
+          heapq.heappush(gifts,cal_val )
+          k-=1
+       gifts=[ -val for val in gifts]
+
+       return sum(gifts)
+
+        
+
+           
+
+
+
+
+
+
+
         
