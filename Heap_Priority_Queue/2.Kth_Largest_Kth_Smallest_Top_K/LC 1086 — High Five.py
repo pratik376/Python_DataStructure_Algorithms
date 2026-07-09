39e0b43739658f252f26cff569b5f99374cb69f6
pratik376 <babariyapratik208@gmail.com>
2026-07-09 16:10:02 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 1086 — High Five.py
+++ Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 1086 — High Five.py
@@ -6,10 +6,34 @@ class Solution:
  def highFive(self, items: List[List[int]]) -> List[List[int]]:
   
   score_dict= defaultdict(list)
+  answer=List[List[int]]
 
   for student in items:
    score_dict[student[0]].append(student[1])
-   
+
+   for key, val in score_dict:
+
+    heapq.heapify_max(val)
+
+    count=5
+    sum=0
+
+    while count>0:
+     sum+=heapq.heappop_max(val)
+     k-=1
+    
+    answer.append([key, sum/5])
+
+    
+     
+
+
+      
+
+
+
+
+
   
 
 
