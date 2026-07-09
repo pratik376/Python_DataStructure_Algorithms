2aee3fe519c826602da35de4d0aaaedc49f80764
pratik376 <babariyapratik208@gmail.com>
2026-07-08 19:33:11 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/heapify.py
+++ Heap_Priority_Queue/heapify.py
@@ -21,16 +21,21 @@ def heapify_max(nums,n,index):
 
 def heapify_max(nums,n,index):
 
-    largest=index
-    left_child_index= 2 * index +1
-    right_child_index= 2 * index +2
+    while True:
 
-    if left_child_index < n and nums[left_child_index] > nums[largest]:
-        largest=left_child_index
-    
-    if right_child_index < n and nums[right_child_index] > nums[largest]:
-        largest= right_child_index
-    
-    if largest != index:
-        nums[largest], nums[index]= nums[index], nums[largest]
-        heapify_max(nums,n,largest)
+        largest=index
+        left_child_index= 2 * index +1
+        right_child_index= 2 * index +2
+
+        if left_child_index < n and nums[left_child_index] > nums[largest]:
+            largest=left_child_index
+        
+        if right_child_index < n and nums[right_child_index] > nums[largest]:
+            largest= right_child_index
+        
+        if largest== index:
+            break
+        
+        if largest != index:
+            nums[largest], nums[index]= nums[index], nums[largest]
+            index= largest
