57c9b657593072e0726acfc854a18e203d3232b5
pratik376 <babariyapratik208@gmail.com>
2026-07-10 16:11:07 -0700

    Add new DSA practice files

---
+++ Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 973 — K Closest Points to Origin.py
@@ -0,0 +1,14 @@
+
+
+from typing import List
+import heapq
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        mini_heap=[]
+
+        
+
+        
