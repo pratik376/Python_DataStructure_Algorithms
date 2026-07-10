57383ef358340186233199e242da8a627e8ac340
pratik376 <babariyapratik208@gmail.com>
2026-07-10 16:16:32 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 973 — K Closest Points to Origin.py
+++ Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 973 — K Closest Points to Origin.py
@@ -2,13 +2,25 @@
 
 from typing import List
 import heapq
-
+import math
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
 
         mini_heap=[]
 
-        
+        for point in points:
+
+            x,y= point
+
+            sq_distance= math.sqrt( x ** 2 + y **2)
+
+            heapq.heappush(mini_heap, (sq_distance,point))
+
+            if len(mini_heap) > k:
+                heapq.heappop(mini_heap)
+        return mini_heap[0]
+
+
 
         
