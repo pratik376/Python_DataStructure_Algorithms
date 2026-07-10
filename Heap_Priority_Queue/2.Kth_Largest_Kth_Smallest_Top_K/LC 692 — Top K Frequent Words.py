27f672a33fa909b6a0a384a786864e229f10e6f9
pratik376 <babariyapratik208@gmail.com>
2026-07-09 17:28:42 -0700

    Add new DSA practice files

---
+++ Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 692 — Top K Frequent Words.py
@@ -0,0 +1,26 @@
+from typing import List
+import heapq
+from collections import defaultdict, Counter
+
+
+class Solution:
+    def topKFrequent(self, nums: List[str], k: int) -> List[int]:
+
+        freq=Counter(nums)
+        mini_heap =[]
+
+
+        for string,count in freq.items():
+
+            heapq.heappush(mini_heap,(count,string))
+
+            if len(mini_heap) > k:
+                heapq.heappop(mini_heap)
+        
+        return [num for count, num in mini_heap]
+            
+        
+        
+        
+        
+        
