8a831d81ab34e0c22ff797c5c0b3f52169411b3c
pratik376 <babariyapratik208@gmail.com>
2026-07-10 17:44:15 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 239 — Sliding Window Maximum.py
+++ Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 239 — Sliding Window Maximum.py
@@ -15,9 +15,32 @@ class Solution:
            heapq.heappush(heap,(-nums[i],i))
            while (heap[0][1] + (k-1) ) < i:
                    heapq.heappop(heap)
-
            if i >= k-1:
                
                answer.append(-heap[0][0])
         
        return answer
+
+class Solution:
+    def minSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+
+        
+
+       heap=[]
+       answer=[]
+
+       for i in range(len(nums)):
+           
+           
+           heapq.heappush(heap,(nums[i],i))
+           while (heap[0][1] + (k-1) ) < i:
+                   heapq.heappop(heap)
+           if i >= k-1:
+               
+               answer.append(heap[0][0])
+        
+       return answer
+    
+
+# for the range 
+#return maxSlidingWindow () - minSlidingWindow 
---
+++ Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 480 — Sliding Window Median.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        
