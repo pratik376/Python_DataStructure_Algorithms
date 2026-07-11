bf0704a72eac91f1100b14d09b427411fb7fced6
pratik376 <babariyapratik208@gmail.com>
2026-07-10 17:51:46 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 480 — Sliding Window Median.py
+++ Heap_Priority_Queue/2.Kth_Largest_Kth_Smallest_Top_K/LC 480 — Sliding Window Median.py
@@ -1,6 +1,20 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         
+       heap=[]
+       answer=[]
+
+       for i in range(len(nums)):
+           
+           
+           heapq.heappush(heap,(nums[i],i))
+           while (heap[0][1] + (k-1) ) < i:
+                   heapq.heappop(heap)
+           if i >= k-1:
+               
+               answer.append(heap[0][0])
+        
+       return answer
