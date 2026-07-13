8f6481cb4e75f083308848c7c6e9d28e872d399e
pratik376 <babariyapratik208@gmail.com>
2026-07-13 16:49:20 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 3264 — Final Array State After K Multiplication Operations I.py
+++ Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 3264 — Final Array State After K Multiplication Operations I.py
@@ -4,15 +4,18 @@ import heapq
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
 
-        heapq.heapify(nums)
+        heapq.heapify(nums) # O(n)
 
-        for _ in range(k):
+        for _ in range(k):  # O(k)
 
-            element=heapq.heappop(nums)
+            element=heapq.heappop(nums) # log(n)
 
-            heapq.heappush(nums, multiplier * element)
+            heapq.heappush(nums, multiplier * element) # log(n)
         
         return nums
+    
+    # space complexity O(1)
+    # time complexity O(n + 2k log(n) ) ==> 0(n+ klongn)
 
 
         
