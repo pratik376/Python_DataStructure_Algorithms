06bda45e193272a53a0bff346a1c0d4e6c002915
pratik376 <babariyapratik208@gmail.com>
2026-07-13 17:42:57 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 2530 — Maximal Score After Applying K Operations.py
+++ Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 2530 — Maximal Score After Applying K Operations.py
@@ -1,6 +1,17 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
+
+        heap= [ -val for val in nums]
+
+        heapq.heapify(heap)
+
+        score=0
+
+        for _ in range(k):
+            
+
+
         
