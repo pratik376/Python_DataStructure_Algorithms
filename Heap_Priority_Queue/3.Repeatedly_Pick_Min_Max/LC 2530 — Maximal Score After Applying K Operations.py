9fad5648bb818c38f7944fe404ed55467b2ea1fd
pratik376 <babariyapratik208@gmail.com>
2026-07-13 17:48:33 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 2530 — Maximal Score After Applying K Operations.py
+++ Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 2530 — Maximal Score After Applying K Operations.py
@@ -1,5 +1,6 @@
 from typing import List
 import heapq
+import math
 
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
@@ -11,7 +12,18 @@ class Solution:
         score=0
 
         for _ in range(k):
-            
+
+            element= -heapq.heappop(heapq)
+
+            score+= element
+
+            heapq.heappush(heap, - (math.ceil(element/3)))
+        
+        return score
+
+
+
+
 
 
         
