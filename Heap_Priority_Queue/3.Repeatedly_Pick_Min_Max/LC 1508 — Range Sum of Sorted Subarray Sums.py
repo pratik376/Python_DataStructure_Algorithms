ad3c9eaca1123e10f0e467b06f23f16b85d06378
pratik376 <babariyapratik208@gmail.com>
2026-07-13 18:25:17 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 1508 — Range Sum of Sorted Subarray Sums.py
+++ Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 1508 — Range Sum of Sorted Subarray Sums.py
@@ -3,4 +3,24 @@ from typing import List
 
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+
+        MOD= 10 ** 9+ 7
+        subarra_sum=[]
+
+        for i in  range(n):
+
+            curr_sum=0
+
+            for j in range(n):
+
+                curr_sum = (curr_sum + nums[j]) % MOD
+                subarra_sum.append(curr_sum)
+        
+        subarra_sum.sort()
+
+        res=0
+
+        for i in range(left-1, right):
+            res= (res + subarra_sum[i])% MOD
+        return res
         
