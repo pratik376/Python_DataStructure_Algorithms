84ab9f7a003de96faccddb2032604443813ce525
pratik376 <babariyapratik208@gmail.com>
2026-07-15 16:23:02 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 1675 — Minimize Deviation in Array.py
+++ Heap_Priority_Queue/3.Repeatedly_Pick_Min_Max/LC 1675 — Minimize Deviation in Array.py
@@ -1,3 +1,21 @@
+from typing import List
+import heapq
+
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
+        
+        max_heap= []
+
+        for num in nums:
+
+            if num % 2 == 0:
+                num= -num   
+            else:
+                num *=2
+
+            heapq.heappush(max_heap,num)
+
+        min_deviation= float('inf')
+        min_val= - max(max_heap)
+
         
