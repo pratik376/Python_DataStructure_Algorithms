84fccc47cd267fea3a52a512578e743be0b85640
pratik376 <babariyapratik208@gmail.com>
2026-07-15 17:45:25 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/4.Minimum_Cost_Merge_Connect_Ropes/GS-array.py
+++ Heap_Priority_Queue/4.Minimum_Cost_Merge_Connect_Ropes/GS-array.py
@@ -7,8 +7,7 @@ class Solution:
         
         num1=0
         num2=0
-        mul_factor=1
-
+        
 
         heapq.heapify(arr)
 
@@ -17,12 +16,10 @@ class Solution:
             n1= heapq.heappop(arr)
             n2= heapq.heappop(arr)
 
-            num1 += (mul_factor *n1)
-            num2 +=(mul_factor *n2)
-
-            mul_factor *=10
+            num1 = num1 *10 + n1
+            num2 =num2 *10 +n2        
         n1= heapq.heappop(arr)
-        num1 += (mul_factor *n1)
+        num1 = num1 * 10 + n1
 
         return num1 + num2
 
