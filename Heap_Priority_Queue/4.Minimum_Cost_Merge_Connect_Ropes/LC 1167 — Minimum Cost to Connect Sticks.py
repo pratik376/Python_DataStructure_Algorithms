273be46ee16f606f0bfa9f80ff1477ef8db1df02
pratik376 <babariyapratik208@gmail.com>
2026-07-15 17:01:07 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/4.Minimum_Cost_Merge_Connect_Ropes/LC 1167 — Minimum Cost to Connect Sticks.py
+++ Heap_Priority_Queue/4.Minimum_Cost_Merge_Connect_Ropes/LC 1167 — Minimum Cost to Connect Sticks.py
@@ -3,4 +3,17 @@ import heapq
 
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
+
+        cost=0
+        heapq.heapify(sticks)
+
+        while len(sticks)> 1:
+
+            s1= heapq.heappop(sticks)
+            s2=heapq.heappop(sticks)
+
+            s3= s1+s2
+
+            cost+=s3
+            
         
