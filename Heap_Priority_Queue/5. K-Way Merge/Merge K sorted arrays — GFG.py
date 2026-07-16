c22b470c29fa7bb36bcbf5ef4e17298c0e9f8ada
pratik376 <babariyapratik208@gmail.com>
2026-07-16 16:58:05 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/5. K-Way Merge/Merge K sorted arrays — GFG.py
+++ Heap_Priority_Queue/5. K-Way Merge/Merge K sorted arrays — GFG.py
@@ -1,4 +1,29 @@
+import heapq
 class Solution:
     def mergeArrays(self, mat):
+
+        heap=[]
+        answer=[]
+
+        for i in range(len(mat)):
+
+            heapq.heappush(heap, (mat[i][0],i,0))  # (element, array_index, position_of_element)
+
+        while heap:
+
+            element, array_index, position_of_element= heapq.heappop(heap)
+
+            answer.append(element)
+
+            if position_of_element + 1 < len(mat[array_index]):
+                heapq.heappush(heap, (mat[array_index][position_of_element + 1],array_index,position_of_element + 1))
+        
+        return answer
+
+
+
+
+
+
         
         
