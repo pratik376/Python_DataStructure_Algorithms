7d3ce1c352cc9ee83dec832b3bb53cb949ddec08
pratik376 <babariyapratik208@gmail.com>
2026-07-15 17:27:06 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/4.Minimum_Cost_Merge_Connect_Ropes/GS-array.py
+++ Heap_Priority_Queue/4.Minimum_Cost_Merge_Connect_Ropes/GS-array.py
@@ -1,5 +1,11 @@
 from typing import List
+import heapq
 
 class Solution:
     def minSum(self, arr: List[int]) -> int:
-        pass
+        
+        cost=0
+
+        heapq.heapify(arr)
+
+        while 
