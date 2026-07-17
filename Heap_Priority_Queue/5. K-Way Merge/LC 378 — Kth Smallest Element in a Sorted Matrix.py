230c078a2402c99a1c862d2ffc1b0c0e84fb5a9d
pratik376 <babariyapratik208@gmail.com>
2026-07-16 18:28:56 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/5. K-Way Merge/LC 378 — Kth Smallest Element in a Sorted Matrix.py
+++ Heap_Priority_Queue/5. K-Way Merge/LC 378 — Kth Smallest Element in a Sorted Matrix.py
@@ -3,5 +3,29 @@ import heapq
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+
+        heap=[]
+        answer=0
+
+        for i in range(len(matrix)):
+
+            heapq.heappush(heap, (matrix[i][0], i, 0))  # value, array number, index
+
+        while len(matrix) - k > 0:
+
+            value, array_number, index= heapq.heappop(heap)
+
+            answer=value
+
+            if index+1 < len(matrix[array_number]):
+                heapq.heappush(heap, (matrix[array_number][index+1], index))
+            
+
+            k-=1
         
+        return answer
+        
+        
+
+
         
