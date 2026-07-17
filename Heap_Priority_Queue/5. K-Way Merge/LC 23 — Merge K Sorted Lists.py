ba3499ac9a6cdb21a4ce231240b74e5fda6f6603
pratik376 <babariyapratik208@gmail.com>
2026-07-17 15:43:49 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/5. K-Way Merge/LC 23 — Merge K Sorted Lists.py
+++ Heap_Priority_Queue/5. K-Way Merge/LC 23 — Merge K Sorted Lists.py
@@ -8,7 +8,41 @@ class ListNode:
         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        prev=None
+        heap=[]
+        head=None
+
+        for i in range(len(lists)):
+
+            heapq.heappush(heap, (lists[i][0].val,lists[i][0],i,0))  # value ,node, list number, node_nuber
         
+        while heap:
+
+            val, curr, list_index, node_number = heapq.heappop(heap)
+
+            if not head:
+                head=curr
+            
+            if not prev:
+                curr=prev
+            else:
+                prev.next = curr
+                prev=curr
+            
+            if curr.next:
+
+                heapq.heappush(heap, (lists[list_index][node_number+1].val,lists[list_index][node_number+1], list_index, node_number +1))
+        
+        return head
+            
+
+            
+
+        
+
+
+
 
 
 
