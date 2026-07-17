46af001285d6aae8e5ad7c3f4d8c91672f21cadd
pratik376 <babariyapratik208@gmail.com>
2026-07-16 17:42:55 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/5. K-Way Merge/LC 373 — Find K Pairs with Smallest Sums.py
+++ Heap_Priority_Queue/5. K-Way Merge/LC 373 — Find K Pairs with Smallest Sums.py
@@ -1,4 +1,5 @@
 from typing import List
+import heapq
 
 
 class Solution:
@@ -7,4 +8,22 @@ class Solution:
         heap=[]
 
         answer=[]
+
+        for i in range(min(len(nums1,nums2))):
+            heapq.heappush((nums1[i] + nums2[i], i,i))
         
+
+        while k:
+
+            sum, i,j = heapq.heappop(heap)
+
+            answer.append([i,j])
+
+            if nums1[i] < nums2[j] and j + 1 < len(nums2):
+
+                heapq.heappush(heap, (nums1[i] + nums1[j+1], i,j+1))
+            
+            elif nums2[j] < nums1[i]  and i+ 1 < len(nums1):
+                heapq.heappush(heap, (nums1[i] + nums1[j+1], i+1,))
+
+
