326d648cd58cac52bb765f0e3408c7e179f2cad5
pratik376 <babariyapratik208@gmail.com>
2026-07-17 16:47:57 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/5. K-Way Merge/LC 632 — Smallest Range Covering Elements from K Lists.py
+++ Heap_Priority_Queue/5. K-Way Merge/LC 632 — Smallest Range Covering Elements from K Lists.py
@@ -1,6 +1,12 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
         
+        k= len(nums)
+
+        left=right= nums[0][0]
+
+        for i in range(k):
+            
