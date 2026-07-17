e117d35ffaa0337ede85cf129c75131c66ef188a
pratik376 <babariyapratik208@gmail.com>
2026-07-16 18:10:44 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/5. K-Way Merge/LC 373 — Find K Pairs with Smallest Sums.py
+++ Heap_Priority_Queue/5. K-Way Merge/LC 373 — Find K Pairs with Smallest Sums.py
@@ -4,6 +4,9 @@ import heapq
 
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+
+        if not nums1 or not nums2 or k==0:
+            return []
         
         heap=[]
 
