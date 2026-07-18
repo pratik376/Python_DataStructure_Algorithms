7df3d0a5cc5ff1ad23dd80bd113bed9f3a9888c7
pratik376 <babariyapratik208@gmail.com>
2026-07-17 17:02:40 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/5. K-Way Merge/LC 632 — Smallest Range Covering Elements from K Lists.py
+++ Heap_Priority_Queue/5. K-Way Merge/LC 632 — Smallest Range Covering Elements from K Lists.py
@@ -15,5 +15,27 @@ class Solution:
 
             left= min(left, l[0])
             right= max(right, l[0])
+            heapq.heappush(heap, (l[0], i, 0)) # (element, index, element_index)
+        
+        res=[left, right]
 
+        while True:
+
+            n, i ,idx= heapq.heappop(heap)
+
+            idx+=1
+
+            if idx == len(nums[i]):
+                break
+
+            next_val= nums[i][idx]
+
+            heapq.heappush(heap, (next_val,i,idx))
+
+            right= max(right, next_val)
+            left= heap[0][0]
+
+            if right - left < res[1] - res[0]:
+                res=[left, right]
+        return res
 
