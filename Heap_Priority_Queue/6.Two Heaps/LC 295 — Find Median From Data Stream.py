edf6e2cf34d50ff9292da6bd53d39f7d618df2f1
pratik376 <babariyapratik208@gmail.com>
2026-07-18 15:07:34 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/6.Two Heaps/LC 295 — Find Median From Data Stream.py
+++ Heap_Priority_Queue/6.Two Heaps/LC 295 — Find Median From Data Stream.py
@@ -3,11 +3,34 @@ class MedianFinder:
 
     def __init__(self):
         self.max_heap=[] # left
-        self.mean_heap=[] #right
+        self.min_heap=[] #right
         
 
     def addNum(self, num: int) -> None:
+
+        if not self.max_heap or num <= -self.max_heap[0]:
+            heapq.heappush(self.max_heap,-num)
+        else:
+            heapq.heappush(self.min_heap, num)
         
+        if abs(len(self.max_heap) - len(self.min_heap)) > 1 :
+
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
 
     def findMedian(self) -> float:
+
+        total_length= len(self.min_heap) + len(self.max_heap)
+        answer=0
+
+        if total_length %2 == 0:
+            num1= -self.max_heap[0]
+            num2= self.min_heap[0]
+
+            answer= (num1 + num2)/2
+        
+        else:
+            answer= -self.max_heap[0]
+        
+        return answer
+        
         
