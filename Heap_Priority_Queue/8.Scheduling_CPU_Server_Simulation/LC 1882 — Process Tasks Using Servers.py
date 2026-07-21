00975d3ed05f7873e0fc658c4a505c40c89f26ad
pratik376 <babariyapratik208@gmail.com>
2026-07-20 19:10:47 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1882 — Process Tasks Using Servers.py
+++ Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1882 — Process Tasks Using Servers.py
@@ -1,3 +1,8 @@
+from typing import List
+
+
 class Solution:
     def assignTasks(self, servers: List[int], tasks: List[int]) -> List[int]:
         
+        # two min heaps, one for available server and one for unavailable
+        #
