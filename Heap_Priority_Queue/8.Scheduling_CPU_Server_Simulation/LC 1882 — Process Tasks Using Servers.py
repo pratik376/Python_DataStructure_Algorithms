e972c2ecf1a8f99d5cc92bbbb2c85ece955de39c
pratik376 <babariyapratik208@gmail.com>
2026-07-20 19:19:17 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1882 — Process Tasks Using Servers.py
+++ Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1882 — Process Tasks Using Servers.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def assignTasks(self, servers: List[int], tasks: List[int]) -> List[int]:
@@ -7,4 +7,29 @@ class Solution:
         # two min heaps, one for available server and one for unavailable
         # available =(weight, index)
         # unavailable = (timeserveBecomesAvailable, weight, index)
-        
+
+        res= [0] * len(tasks)
+
+        available= [(servers[i],i) for i in range(len(servers))]
+        available=heapq.heapify(available)
+        unavailable= []
+        time=0
+
+        for i in range(len(servers)):
+            time= max(i,time)
+
+            if not available:
+                time= unavailable[0][0]
+            
+            while unavailable and time >= unavailable[0][0]:
+                timefree, w, index = heapq.heappop(unavailable)
+                heapq.heappush(available, (w,index))
+            
+            w, index = heapq.heappop(available)
+
+            res[i]=index
+
+            heapq.heappush(unavailable, (time+ tasks[i],w,index))
+        return res
+            
+
