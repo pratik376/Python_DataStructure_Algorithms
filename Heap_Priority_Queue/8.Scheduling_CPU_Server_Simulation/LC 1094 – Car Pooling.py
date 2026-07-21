3e1470bb8714c9bac0f40dbaad5cd24ce1c64510
pratik376 <babariyapratik208@gmail.com>
2026-07-21 16:32:41 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1094 – Car Pooling.py
+++ Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1094 – Car Pooling.py
@@ -4,5 +4,26 @@ from typing import List
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
 
-        # solution 1
+        # solution 1 #O(n)
+
+        passChange =[0] * 1001
+
+        for t in trips:
+            numPass,start, end= t
+
+            passChange[start] += numPass
+            passChange[end]-= numPass
+        
+        curr=0
+
+        for i in range(1001):
+
+            curr+= passChange[i]
+
+            if curr > capacity:
+                return False
+        return True
+    
+    
+  
         
