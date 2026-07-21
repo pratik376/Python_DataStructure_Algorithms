3eaf9e94e10755de20169607d48d95f5df3dfc4f
pratik376 <babariyapratik208@gmail.com>
2026-07-20 17:29:00 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1834 — Single-Threaded CPU.py
+++ Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1834 — Single-Threaded CPU.py
@@ -4,3 +4,9 @@ from typing import List
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         
+
+        for i,t in enumerate(tasks):
+            t.append(i)
+        
+        tasks.sort(key= lambda t: t[0])
+        
