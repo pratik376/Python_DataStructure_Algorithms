e986d17dcd1593cf2216ce5950c069456d241abc
pratik376 <babariyapratik208@gmail.com>
2026-07-20 19:11:43 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1882 — Process Tasks Using Servers.py
+++ Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1882 — Process Tasks Using Servers.py
@@ -5,4 +5,6 @@ class Solution:
     def assignTasks(self, servers: List[int], tasks: List[int]) -> List[int]:
         
         # two min heaps, one for available server and one for unavailable
-        #
+        # available =(weight, index)
+        # unavailable = (timeserveBecomesAvailable, weight, index)
+        
