05031d68060b4593d7d4506e55adacfadfc4334c
pratik376 <babariyapratik208@gmail.com>
2026-07-21 17:49:38 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1057 – Campus Bikes.py
+++ Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1057 – Campus Bikes.py
@@ -1,2 +1,32 @@
 from typing import List
 import heapq
+
+class Solution:
+    def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> List[int]:
+        heap=[]
+
+        for w in range(len(workers)):
+
+            wx,wy= workers[w]
+
+            for b in range(len(bikes)):
+                bx,by= bikes[b]
+
+                dist= abs(wx-bx) + abs(wy-by)
+                heapq.heappush(heap, (dist,w,b))
+            
+
+            res=[-1] * len(workers)
+
+            worker_used=[False] * len(workers)
+            bike_used=[False]* len(bikes)
+
+            while heap:
+
+                dist, w, b= heapq.heappop(heap)
+
+                if not worker_used[w] and not bike_used[b]:
+                    worker_used[w]=True
+                    res[w]=b
+                    bike_used[b]=True
+            return res
