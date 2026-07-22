8a72c5b11269edc8e69df1c57e612a92ba2e3c93
pratik376 <babariyapratik208@gmail.com>
2026-07-21 19:26:59 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1942 — The Number of the Smallest Unoccupied Chair.py
+++ Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 1942 — The Number of the Smallest Unoccupied Chair.py
@@ -4,5 +4,37 @@ import heapq
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
 
-        
+        for i in range(times):
+            times[i].append(i)
+
+        times.sort(lambda x : x[0])
+
+        availbe_chair=[i for i in range(times)]
+        busy_chair=[]
+
+        for time in times:
+
+            arrival, end, position= time
+
+            while busy_chair and arrival>=busy_chair[0][0]:
+                end_time, chair_number=heapq.heappop(busy_chair)
+                heapq.heappush(availbe_chair,chair_number)
+
+            chair_number= heapq.heappop(availbe_chair)
+
+            if position==targetFriend:
+                return 
+
+            heapq.heappush(busy_chair, (end, chair_number))
+
+
+
+
+
+            
+
+
+         
+
+
         
