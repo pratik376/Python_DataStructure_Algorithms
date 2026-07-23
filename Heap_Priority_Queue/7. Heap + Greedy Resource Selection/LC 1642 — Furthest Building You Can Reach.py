a5490480f28e4dbf76d4d2102ea0845387490bb8
pratik376 <babariyapratik208@gmail.com>
2026-07-23 15:44:16 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 1642 — Furthest Building You Can Reach.py
+++ Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 1642 — Furthest Building You Can Reach.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
@@ -10,7 +10,13 @@ class Solution:
         for i in range(len(heights)-1):
 
             diff= heights[i+1] - heights[i]
-            
+
+            if diff <=0:
+                continue
+
+            bricks-=diff
+            heapq.heappush()
+
 
 
 
