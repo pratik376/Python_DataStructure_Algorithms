0899224db62bd8ffaf523378b727b7705003406f
pratik376 <babariyapratik208@gmail.com>
2026-07-22 17:04:30 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 2402 — Meeting Rooms III.py
+++ Heap_Priority_Queue/8.Scheduling_CPU_Server_Simulation/LC 2402 — Meeting Rooms III.py
@@ -1,6 +1,32 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+
+        avalible_rooms= [(i,0) for i in range(len(n))]  # (room_number, metting_number)
+        busy_rooms=[] # ( free_time, room_number, metting_number)
+
+        meetings.sort(key=lambda x: x[0])
+
+        for i in range(len(meetings)):
+
+            start_time, end_time= meetings[i]
+
+
+            while busy_rooms and start_time >= busy_rooms[0][0]:
+               freeTime, room_num, meeting_number =heapq.heappop(busy_rooms)
+               heapq.heappush(avalible_rooms,meeting_number)
+
+            if not avalible_rooms:
+                freeTime, room_num, meeting_number =heapq.heappop(busy_rooms)
+
+
+
+         
+            room_num, meeting_number=heapq.heappop(avalible_rooms)
+
+            heapq.heappush(busy_rooms, (end_time,room_num, meeting_number+1))
+        
+                
         
