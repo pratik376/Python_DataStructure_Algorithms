3fe7a38c1fb536e735af28ca00c9e3d3633b842d
pratik376 <babariyapratik208@gmail.com>
2026-07-23 18:38:04 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 2542 — Maximum Subsequence Score.py
+++ Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 2542 — Maximum Subsequence Score.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
-    def `maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
         
