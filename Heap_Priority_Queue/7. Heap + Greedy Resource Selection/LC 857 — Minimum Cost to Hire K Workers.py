f70c15f58edee7285b23b7568d96175fdea3bfa0
pratik376 <babariyapratik208@gmail.com>
2026-07-24 16:53:20 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 857 — Minimum Cost to Hire K Workers.py
+++ Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 857 — Minimum Cost to Hire K Workers.py
@@ -12,6 +12,26 @@ class Solution:
 
         pairs.sort()
 
+        maxHeap=[]
+
+        total_quality=0
+
+        for rate, q in pairs:
+            heapq.heappush(maxHeap, -q)
+            total_quality += q
+
+            if len(maxHeap)> k:
+                total_quality += heapq.heappop(maxHeap)
+
+            if len(maxHeap)==k:
+
+                res= min(
+                    res,
+                    total_quality * rate
+                )
+
+        return res
+
 
 
         
