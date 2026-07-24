e777a487083a2ee5bae9ff7338dc483db6bfe770
pratik376 <babariyapratik208@gmail.com>
2026-07-23 19:06:11 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 2542 — Maximum Subsequence Score.py
+++ Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 2542 — Maximum Subsequence Score.py
@@ -1,8 +1,17 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
         pairs= [(n1,n2) for n1,n2 in zip(nums1, nums2)]
         pairs= sorted(pairs, key= lambda x : x[1], reverse=True)
-        
+
+        minHeap=[]
+        n1Sum=0
+        res=0
+
+        for n1,n2 in pairs:
+
+            nums1+= n1
+            heapq.heappush(minHeap,n1)
+            
