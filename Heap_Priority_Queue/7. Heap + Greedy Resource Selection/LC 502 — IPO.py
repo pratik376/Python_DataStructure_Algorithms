83f389c4c8b4e260ed37b5f37b8d844d94deab3d
pratik376 <babariyapratik208@gmail.com>
2026-07-24 15:15:26 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 502 — IPO.py
+++ Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 502 — IPO.py
@@ -1,6 +1,12 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+
+        MaxProfit= []
+        minCapital= [(c,p) for c, p in zip(capital,profits)]
+        heapq.heapify(minCapital)
+
+
         
