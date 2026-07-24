4c1727e7ed918e50ee092702d9b6ddd9e6f6f8aa
pratik376 <babariyapratik208@gmail.com>
2026-07-24 16:51:11 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 857 — Minimum Cost to Hire K Workers.py
+++ Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 857 — Minimum Cost to Hire K Workers.py
@@ -3,5 +3,15 @@ import heapq
 
 class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-        
+
+        res= float("inf")
+        pairs=[]
+
+        for i in range(len(quality)):
+            pairs.append( (wage[i]/quality[i], quality[i]))
+
+        pairs.sort()
+
+
+
         
