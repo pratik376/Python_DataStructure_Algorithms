07739c1417959855a65efb37992a58895e2d1cc3
pratik376 <babariyapratik208@gmail.com>
2026-07-25 16:44:02 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 630 — Course Schedule III.py
+++ Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 630 — Course Schedule III.py
@@ -1,6 +1,20 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
+
+        courses.sort( key= lambda x : x[1])
+
+        maxHeap=[]
+
+        totalDays=0
+
+        for duration, lastDay in courses:
+
+            totalDays+=duration
+
+            heapq.heappush(maxHeap, -duration)
+
+            
         
