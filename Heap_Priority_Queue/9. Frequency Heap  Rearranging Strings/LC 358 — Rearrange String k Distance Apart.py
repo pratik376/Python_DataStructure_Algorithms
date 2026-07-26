499acd15fe767ab50f271213819bb591bb0e6e5b
pratik376 <babariyapratik208@gmail.com>
2026-07-26 16:52:04 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 358 — Rearrange String k Distance Apart.py
+++ Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 358 — Rearrange String k Distance Apart.py
@@ -1,3 +1,9 @@
+from typing import List
+from collections import Counter
+
 class Solution:
     def rearrangeBarcodes(self, barcodes: List[int]) -> List[int]:
+
+        count= Counter(barcodes)
+        
         
