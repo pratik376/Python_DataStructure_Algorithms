72539d4cb5e469c29c607d248d37982f95962c84
pratik376 <babariyapratik208@gmail.com>
2026-07-25 17:48:13 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 871 — Minimum Number of Refueling Stops.py
+++ Heap_Priority_Queue/7. Heap + Greedy Resource Selection/LC 871 — Minimum Number of Refueling Stops.py
@@ -1,4 +1,27 @@
 import heapq
+from typing import List
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+
+        Maxheap=[]
+
+        stations.sort() 
+
+        current_mile=0
+
+        for position, fuel in stations:
+
+            startFuel-=position
+            startFuel += fuel
+            current_mile+= position
+            heapq.heappush(heap,fuel)
+            
+
+
+            if current_mile >= target:
+                return len(heap)
+
+            if current_mile <
+
+
         
