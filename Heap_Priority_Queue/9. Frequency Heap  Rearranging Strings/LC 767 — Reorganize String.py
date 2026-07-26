c13b37c00557414df25d1e3193ecbd5eb83a3375
pratik376 <babariyapratik208@gmail.com>
2026-07-25 19:13:23 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 767 — Reorganize String.py
+++ Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 767 — Reorganize String.py
@@ -1,3 +1,40 @@
+from collections import Counter
+import heapq
+
 class Solution:
     def reorganizeString(self, s: str) -> str:
+
+        freq= Counter(s)
+
+        Maxheap= [(-value,key)  for key, value in freq.items()]
+
+        heapq.heapify(Maxheap)
+        res=[]
+
+        prev_char=''
+        prev_freq=0
+
+
+        while Maxheap:
+
+ 
+
+            value, char =heapq.heappop(Maxheap)
+
+            if prev_char is not '' and prev_freq:
+                heapq.heappush(Maxheap,(prev_freq,prev_char))
+
+            res.append(char)
+            value+=1
+
+            prev_char=char
+            prev_freq=value
+        
+
+
+
+
+
+
+
         
