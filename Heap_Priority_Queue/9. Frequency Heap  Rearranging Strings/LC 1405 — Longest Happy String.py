f56d21bc9eec9f0167c664b6e763c1b1031d0d3b
pratik376 <babariyapratik208@gmail.com>
2026-07-26 15:53:18 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 1405 — Longest Happy String.py
+++ Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 1405 — Longest Happy String.py
@@ -20,10 +20,10 @@ class Solution:
             freq, char =heapq.heappop(Maxheap)
 
             if freq >= 2:
-                freq-=2
+                freq+=2
                 res += char *2
             elif freq>0:
-                freq-=1
+                freq+=1
                 res+= char
 
             if prev_char !='' and prev_freq:
