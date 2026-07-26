bca1bbbda1b224caa2d86c089060410d80188d7a
pratik376 <babariyapratik208@gmail.com>
2026-07-26 16:24:59 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 1405 — Longest Happy String.py
+++ Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 1405 — Longest Happy String.py
@@ -5,9 +5,13 @@ import heapq
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
 
-        arr= [('a', a),('b',b),('c',c)]
+        Maxheap=[]
+        arr= [(-a, 'a'),(-b,'b'),(-c,'c')]
 
-        Maxheap = [(-fre,char) for char, fre in arr]
+        for count, char in arr:
+
+            if count:
+                heapq.heappush(Maxheap,(count,char))
 
         heapq.heapify(Maxheap)
 
@@ -19,11 +23,30 @@ class Solution:
             freq, char =heapq.heappop(Maxheap)
             freq= -freq
 
-            
+            if len(res)>=2 and res[-1]==res[-2]==char:
 
-     
+                if not Maxheap:
+                    break
 
-        return res
+                freq2, char2= heapq.heappop(Maxheap)
+                freq2=-freq2
+
+                res.append(freq2)
+                freq2-=1
+
+                if freq2 >0:
+                    heapq.heappush(Maxheap,(freq2,char2))
+
+                heapq.heappush(Maxheap,(-freq,char))
+
+            else:
+                res.append(char)
+                freq-=1
+
+                if freq:
+                    heapq.heappush(Maxheap,(freq,char))
+    
+        return ''.join(res)
 
             
             
