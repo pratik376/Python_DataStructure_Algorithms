52320e410896e4ceee4dc3c30105194e383abb5d
pratik376 <babariyapratik208@gmail.com>
2026-07-26 17:35:57 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 358 — Rearrange String k Distance Apart.py
+++ Heap_Priority_Queue/9. Frequency Heap  Rearranging Strings/LC 358 — Rearrange String k Distance Apart.py
@@ -12,5 +12,29 @@ class Solution:
         max_heap= [(-count,char) for char,count in freq.items()]
 
         heapq.heapify(max_heap)
-        
+
+        q= deque()
+
+        res=[]
+
+        for i in range(s):
+
+            while q and q[0][0] <= i:
+               _,count,char =q.popleft()
+               heapq.heappush(max_heap,(count,char))
+
+            if not max_heap:
+                return ""
+
+            count, char= heapq.heappop(max_heap)
+            res.append(char)
+            count+=1
+
+            if count<=0:
+                q.append((i+k,count,char))
+
+        return "".join(res)
+
+            
+
 
