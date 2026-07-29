74a94fec7b431d0407637b8f99907cf35d1a39aa
pratik376 <babariyapratik208@gmail.com>
2026-07-28 19:14:01 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/10. Heap + Intervals  Events  Queries/LC 253 — Meeting Rooms II.py
+++ Heap_Priority_Queue/10. Heap + Intervals  Events  Queries/LC 253 — Meeting Rooms II.py
@@ -1,6 +1,55 @@
 from typing import List
 import heapq
 
+class Interval:
+    def __init__(self,start,end):
+        self.start= start
+        self.end= end
+
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        
+
+
+        start= sorted([i.start for i in intervals])
+        end= sorted([i.end for i in intervals])
+        res ,count = 0,0 
+
+        s,e = 0,0 
+
+        while s < len(intervals):
+
+            if start[s] < end[e]:
+                s+=1
+                count+=1
+            else:
+                e+=1
+                count-=1
+
+            res = max(res,count)
+
+        return res
+
+
+            
+         
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+
+        if not intervals:
+            return 0
+
+        intervals.sort()
+        end_heap=[]
+        rooms=0
+
+        for star, end in intervals:
+
+            while end_heap and end_heap[0] <=star:
+                heapq.heappop(end_heap)
+
+            heapq.heappush(end_heap, end)
+
+            rooms= max(rooms, len(end_heap))
+
+        return rooms
+
