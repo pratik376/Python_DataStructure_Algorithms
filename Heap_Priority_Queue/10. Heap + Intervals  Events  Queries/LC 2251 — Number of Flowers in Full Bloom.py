0ce1fe09c400b577853c98e13fbeb52ff1af95b1
pratik376 <babariyapratik208@gmail.com>
2026-07-28 17:39:02 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/10. Heap + Intervals  Events  Queries/LC 2251 — Number of Flowers in Full Bloom.py
+++ Heap_Priority_Queue/10. Heap + Intervals  Events  Queries/LC 2251 — Number of Flowers in Full Bloom.py
@@ -1,4 +1,39 @@
 import heapq
+from typing import List
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+
+        
+        flowers.sort()
+
+        sorted_people =  sorted((time,idx) for idx, time in enumerate(people))
+
+        answer= [] 
+        ending_time_heap=[]
+        answer=[0] * len(people)
+
+        i=0
+
+
+        for time,idx in sorted_people:
+
+            while i < len(flowers) and time <=flowers[i][0]:
+
+                heapq.heappush(ending_time_heap, flowers[i][1])
+                i+=1
+
+            while ending_time_heap and ending_time_heap[0] < time:
+                heapq.heappop(ending_time_heap)
+
+            answer[idx]= len(ending_time_heap)
+
+        return answer 
+
+
+
+
+
+
+
+
         
