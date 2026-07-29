2ca6675318beba464c230a530827152000a1cd1d
pratik376 <babariyapratik208@gmail.com>
2026-07-28 18:27:13 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/10. Heap + Intervals  Events  Queries/LC 1851 — Minimum Interval to Include Each Query.py
+++ Heap_Priority_Queue/10. Heap + Intervals  Events  Queries/LC 1851 — Minimum Interval to Include Each Query.py
@@ -18,12 +18,11 @@ class Solution:
         for val, index in sorted_query:
 
             while i < len(intervals) and intervals[i][0]<=val:
-                heapq.heappush(min_heap, (intervals[i][1],intervals[i][1] - intervals[i][0] +1 ))
+                heapq.heappush(min_heap, (intervals[i][1] - intervals[i][0] +1 ,intervals[i][1]))
                 i+=1
 
             while min_heap and min_heap[0][0] < val:
                 heapq.heappop(min_heap)
-
 
             if min_heap:
                 answer[index]= min_heap[0][1]
