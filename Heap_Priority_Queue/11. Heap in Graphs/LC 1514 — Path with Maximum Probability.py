0afe4a633847bd00a87775be13746dab7e259942
pratik376 <babariyapratik208@gmail.com>
2026-07-29 19:30:08 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/11. Heap in Graphs/LC 1514 — Path with Maximum Probability.py
+++ Heap_Priority_Queue/11. Heap in Graphs/LC 1514 — Path with Maximum Probability.py
@@ -5,4 +5,23 @@ from collections import defaultdict
 
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+
+        edges=defaultdict(list)  # (prob, node)
+        i=0
+
+        for start, des in edges:
+            edges[start].append((succProb[i],des))
+            i+=1
+
+        visited= set()
+        maxHeap= [(1, start_node)] # (prob, node)
+
+
+        while maxHeap:
+            prob, node= heapq.heappop(maxHeap)
+
+            
+
+
+
         
