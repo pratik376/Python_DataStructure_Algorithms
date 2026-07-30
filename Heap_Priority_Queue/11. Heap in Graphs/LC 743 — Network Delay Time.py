49c5317871832839a24e200b5318c6bc7e7e02b8
pratik376 <babariyapratik208@gmail.com>
2026-07-29 17:11:38 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/11. Heap in Graphs/LC 743 — Network Delay Time.py
+++ Heap_Priority_Queue/11. Heap in Graphs/LC 743 — Network Delay Time.py
@@ -1,6 +1,10 @@
 from typing import List
-
+import heapq
+from collections import defaultdict
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+
+        edges= defaultdict(list)
+        
         
