fb6d91fc0085e4761271f2025658c6000e01a36e
pratik376 <babariyapratik208@gmail.com>
2026-07-29 18:35:00 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/11. Heap in Graphs/LC 1631 — Path With Minimum Effort.py
+++ Heap_Priority_Queue/11. Heap in Graphs/LC 1631 — Path With Minimum Effort.py
@@ -4,4 +4,16 @@ from collections import defaultdict
 
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
+
+        ROWS, COLUMS= len(heights), len(heights[0])
+
+        minHeap = [[0,0,0]] # (diff, r,c)
+        visit= set()
         
+        directions= [[0,1], [0,-1], [1,0],[-1,0]]
+
+        while minHeap:
+
+            diff, r,c = heapq.heappop(minHeap)
+            
+
