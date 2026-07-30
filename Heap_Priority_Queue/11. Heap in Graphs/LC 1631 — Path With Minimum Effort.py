4c3986dd08b29d0c37f8731576920a30e13bb971
pratik376 <babariyapratik208@gmail.com>
2026-07-29 18:40:33 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/11. Heap in Graphs/LC 1631 — Path With Minimum Effort.py
+++ Heap_Priority_Queue/11. Heap in Graphs/LC 1631 — Path With Minimum Effort.py
@@ -8,12 +8,30 @@ class Solution:
         ROWS, COLUMS= len(heights), len(heights[0])
 
         minHeap = [[0,0,0]] # (diff, r,c)
-        visit= set()
+        visited= set()
         
         directions= [[0,1], [0,-1], [1,0],[-1,0]]
 
         while minHeap:
 
             diff, r,c = heapq.heappop(minHeap)
-            
+
+            if (r,c) in visited:
+
+                continue
+
+            visited.add((r,c))
+
+            if (r,c)==(ROWS-1, COLUMS-1):
+                return diff
+
+            for dr,dc in directions:
+                newR,newC= r+ dr , c+dc
+
+                if newR < 0 or newC < 0 or newR==ROWS or newC==COLUMS or (newR,newC) in visited:
+                    continue 
+
+                newDiff= max(diff, abs(heights[r][c]-heights[newR][newC]))
+                heapq.heappush(minHeap, [newDiff, newR,newC])
+
 
