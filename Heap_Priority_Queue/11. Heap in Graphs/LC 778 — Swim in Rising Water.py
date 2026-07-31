42d0102f03b0677ff92561cf78ab0749c1553712
pratik376 <babariyapratik208@gmail.com>
2026-07-30 18:44:24 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/11. Heap in Graphs/LC 778 — Swim in Rising Water.py
+++ Heap_Priority_Queue/11. Heap in Graphs/LC 778 — Swim in Rising Water.py
@@ -1,3 +1,16 @@
+from typing import List
+import heapq
+from collections import defaultdict
+
+
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
+
+        ROWS,Columns= len(grid[0]), len(grid[1])
+
+        visited= set()
+
+        minHeap = [[grid[0][0], 0, 0]]  # current_max,row ,column
+        directions= [[0,1], [0,-1], [1,0],[-1,0]]
+        
         
