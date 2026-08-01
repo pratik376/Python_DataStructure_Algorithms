a0a9e15aed0adeeecf4cbfa7a418df6e0c6cb456
pratik376 <babariyapratik208@gmail.com>
2026-08-01 16:46:19 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/12. Heap + Prefix  DP Optimization/LC 1425 — Constrained Subsequence Sum.py
+++ Heap_Priority_Queue/12. Heap + Prefix  DP Optimization/LC 1425 — Constrained Subsequence Sum.py
@@ -5,5 +5,8 @@ import heapq
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
         res=nums[0]
+
+        heap_max=[(nums[0],0)] # max_sum, index
+        
         
         
