0073c7119acbb9e8108582b299bf57c862f37f6c
pratik376 <babariyapratik208@gmail.com>
2026-08-01 23:39:49 -0700

    Add new DSA practice files

--- Heap_Priority_Queue/13. Heap Design Problems/LC 355 — Design Twitter.py
+++ Heap_Priority_Queue/13. Heap Design Problems/LC 355 — Design Twitter.py
@@ -40,7 +40,7 @@ class Twitter:
                 )
 
         return res
-# jkhkhkhk
+# jkhkhkh
     def follow(self, followerId: int, followeeId: int) -> None:
         self.followMap[followerId].add(followeeId)
 
