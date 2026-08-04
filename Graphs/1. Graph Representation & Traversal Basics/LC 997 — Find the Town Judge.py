bb7b2f61e40e9705d7d5d69fd0e0e63b652bf80e
pratik376 <babariyapratik208@gmail.com>
2026-08-03 17:41:17 -0700

    Add new DSA practice files

--- Graphs/1. Graph Representation & Traversal Basics/LC 997 — Find the Town Judge.py
+++ Graphs/1. Graph Representation & Traversal Basics/LC 997 — Find the Town Judge.py
@@ -1,3 +1,18 @@
+from typing import List
+
+
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        
+
+        juge= 0 
+
+        for personA, personB in trust:
+
+            if juge ==0:
+                personB= juge
+
+            elif (juge != personB):
+                return -1
+
+        return juge
+
