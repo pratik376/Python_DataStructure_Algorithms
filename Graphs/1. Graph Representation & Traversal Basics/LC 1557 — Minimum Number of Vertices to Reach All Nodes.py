b24a15b353be3c08e00dd96b091229d79e12df7a
pratik376 <babariyapratik208@gmail.com>
2026-08-03 19:43:49 -0700

    Add new DSA practice files

--- Graphs/1. Graph Representation & Traversal Basics/LC 1557 — Minimum Number of Vertices to Reach All Nodes.py
+++ Graphs/1. Graph Representation & Traversal Basics/LC 1557 — Minimum Number of Vertices to Reach All Nodes.py
@@ -6,10 +6,20 @@ class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
 
         incomingEdge= defaultdict(int)
+        answer=[]
 
 
         for a,b in edges:
 
             incomingEdge[b]+=1
+
+        for i in range(n):
+
+            if incomingEdge[i]==0:
+                answer.append(i)
+
+        return answer
+
+        
         
         
