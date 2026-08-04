3f81c612b834a3d1d8e636862e60890fe8e9768b
pratik376 <babariyapratik208@gmail.com>
2026-08-03 19:19:25 -0700

    Add new DSA practice files

--- Graphs/1. Graph Representation & Traversal Basics/LC 1557 — Minimum Number of Vertices to Reach All Nodes.py
+++ Graphs/1. Graph Representation & Traversal Basics/LC 1557 — Minimum Number of Vertices to Reach All Nodes.py
@@ -25,5 +25,22 @@ class Solution:
 
         
 
-    
+    from typing import List
+
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        incoming = [0] * n
+
+        for src, dst in edges:
+            incoming[dst] += 1
+
+        champions = []
+        for i, incoming_cnt in enumerate(incoming):
+            if incoming_cnt == 0:
+                champions.append(i)
+
+        if len(champions) != 1:
+            return -1
+
+        return champions[0]
         
