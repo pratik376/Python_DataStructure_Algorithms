c8fcb412035ef0c4129db3d518cef377995a817b
pratik376 <babariyapratik208@gmail.com>
2026-08-03 19:10:58 -0700

    Add new DSA practice files

--- Graphs/1. Graph Representation & Traversal Basics/. LC 1557 — Minimum Number of Vertices to Reach All Nodes.py
+++ Graphs/1. Graph Representation & Traversal Basics/. LC 1557 — Minimum Number of Vertices to Reach All Nodes.py
@@ -4,14 +4,12 @@ from collections import defaultdict
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
         inComingEdge= defaultdict(int)
-        outGoingEdge= defaultdict(int)
-
+      
         count=0
         vertex=0
         
-
         for a, b in edges:
-            outGoingEdge[a]+=1
+      
             inComingEdge[b]+=1
 
         for i in range(n):
