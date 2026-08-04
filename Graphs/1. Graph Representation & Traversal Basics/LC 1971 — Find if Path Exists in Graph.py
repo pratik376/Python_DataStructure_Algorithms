c1750d558d3f69c31e444b91e146d3eae3559082
pratik376 <babariyapratik208@gmail.com>
2026-08-04 16:25:47 -0700

    Add new DSA practice files

--- Graphs/1. Graph Representation & Traversal Basics/LC 1971 — Find if Path Exists in Graph.py
+++ Graphs/1. Graph Representation & Traversal Basics/LC 1971 — Find if Path Exists in Graph.py
@@ -1,3 +1,24 @@
+from typing import List
+from collections import defaultdict
+
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+
+        edges= defaultdict(set)
+
+        for a,b in edges:
+
+            edges[a].append(b)
+            edges[b].append(a)
+
+        if destination in edges[source]:
+            return True
+
+        return False 
+
+
+
+
+        
         
