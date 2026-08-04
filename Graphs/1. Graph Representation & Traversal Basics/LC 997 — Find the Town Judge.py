8fe6ae4d969391c996bd2b8d01a71857d81acf14
pratik376 <babariyapratik208@gmail.com>
2026-08-03 18:13:33 -0700

    Add new DSA practice files

--- Graphs/1. Graph Representation & Traversal Basics/LC 997 — Find the Town Judge.py
+++ Graphs/1. Graph Representation & Traversal Basics/LC 997 — Find the Town Judge.py
@@ -16,9 +16,12 @@ class Solution:
 
             inDegreeCount[personB]+=1
             outDegreeCount[personA]+=1
+
+        for i in range(1, n+1):
+
+            if inDegreeCount[i]== n-1 and outDegreeCount[i]==0:
+                return i
         
-
-
         return judge if judge else -1
         
 
