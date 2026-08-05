0ce0a44e56d63a2c3f6bbbd8f12ae44f82dd1c84
pratik376 <babariyapratik208@gmail.com>
2026-08-04 17:58:08 -0700

    Add new DSA practice files

--- Graphs/1. Graph Representation & Traversal Basics/LC 953 — Verifying an Alien Dictionary.py
+++ Graphs/1. Graph Representation & Traversal Basics/LC 953 — Verifying an Alien Dictionary.py
@@ -7,6 +7,20 @@ class Solution:
         ordrInd= {c:i for i,c in enumerate(order)}
 
         for i in range(len(words)-1):
-            
+            w1,w2= words[i], words[i+1]
+
+            for j in range(len(w1)):
+
+                if j==len(w2):
+                    return False
+
+                if w1[j] !=w2[j]:
+
+                    if ordrInd[w2[j]] < ordrInd[w1[j]]:
+                        return False
+
+        return True
+
+
 
         
