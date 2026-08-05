1aef0cb68dd7d5ff1e52505f88d171056fe431eb
pratik376 <babariyapratik208@gmail.com>
2026-08-04 17:55:17 -0700

    Add new DSA practice files

--- Graphs/1. Graph Representation & Traversal Basics/LC 953 — Verifying an Alien Dictionary.py
+++ Graphs/1. Graph Representation & Traversal Basics/LC 953 — Verifying an Alien Dictionary.py
@@ -5,5 +5,8 @@ class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
 
         ordrInd= {c:i for i,c in enumerate(order)}
-        
+
+        for i in range(len(words)-1):
+            
+
         
