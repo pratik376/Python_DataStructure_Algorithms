8c2f05fda67c67a30bfa6e0d27416479bdbcbe7c
pratik376 <babariyapratik208@gmail.com>
2026-08-06 16:00:16 -0700

    Add new DSA practice files

--- Graphs/2. DFS on Graphs/LC 2101 — Detonate the Maximum Bombs.py
+++ Graphs/2. DFS on Graphs/LC 2101 — Detonate the Maximum Bombs.py
@@ -1,3 +1,28 @@
+from typing import List
+from collections import defaultdict
+import math
+
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
+
+        adjList=defaultdict(list)
+
+
+        for i in range(len(bombs)):
+            for j in range(i+1 , range(bombs)):
+
+                x1,y1,r1= bombs[i]
+                x2,y2,r2= bombs[j]
+
+                d= math.sqrt( (x1-x2)** 2 + (y1-y2)**2)
+
+                if d<=r1:
+                    adjList[i].append(j)
+
+                if d<=r2:
+                    adjList[j].append(i)
+
+        
+
+
         
