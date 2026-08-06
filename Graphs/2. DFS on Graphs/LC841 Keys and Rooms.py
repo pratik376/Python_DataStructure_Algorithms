5411a97fb7999be22de4e8aa620723ee994a677b
pratik376 <babariyapratik208@gmail.com>
2026-08-05 18:44:39 -0700

    Add new DSA practice files

--- Graphs/2. DFS on Graphs/LC841 Keys and Rooms.py
+++ Graphs/2. DFS on Graphs/LC841 Keys and Rooms.py
@@ -3,4 +3,20 @@ from typing import List
 
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+
+        dict= {index:val for index, val in enumerate(rooms)}
+
+        stack=[0]
+        seen=set()
+        seen.add(0)
+
+
+        while stack:
+
+
+            room= stack.pop()
+
+
+
+
         
