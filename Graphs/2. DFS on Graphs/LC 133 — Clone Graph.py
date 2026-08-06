b2565fd9bfaf3fda6e12a19dc1af384596839d40
pratik376 <babariyapratik208@gmail.com>
2026-08-05 17:23:14 -0700

    Add new DSA practice files

--- Graphs/2. DFS on Graphs/LC 133 — Clone Graph.py
+++ Graphs/2. DFS on Graphs/LC 133 — Clone Graph.py
@@ -5,31 +5,17 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 
 from typing import Optional
+
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
 
-
-        stack=[node]
-        head= Node(node.val)
-        stack2=[head]
+        if not node:
+            return Node
 
 
 
-        seen=set()
-        seen.add(node)
 
-        while stack:
 
-            node=stack.pop()
-            clone=stack2.pop()
 
-            for neighbour in node.neighbors:
-
-                if not neighbour in seen:
-                    clone_neigh=Node(neighbour.val)
-                    clone.neighbors.append(clone_neigh)
-                    stack2.append(clone_neigh)
-                    seen.add(neighbour)
-                    stack.append(neighbour)
 
         
