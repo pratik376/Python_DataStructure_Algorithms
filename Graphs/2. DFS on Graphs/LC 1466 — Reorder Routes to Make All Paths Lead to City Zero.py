812d05277bb375d5e1f6e44b6e5f3be615fb7d57
pratik376 <babariyapratik208@gmail.com>
2026-08-06 17:25:47 -0700

    Add new DSA practice files

--- Graphs/2. DFS on Graphs/LC 1466 — Reorder Routes to Make All Paths Lead to City Zero.py
+++ Graphs/2. DFS on Graphs/LC 1466 — Reorder Routes to Make All Paths Lead to City Zero.py
@@ -1,3 +1,23 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
+
+        neighbours=defaultdict(list)
+
+        graph= { (a,b) for a,b in connections }
+
+        visited= set()
+
+        answer=0
+
+
+        for a,b in connections:
+            neighbours[a].append(b)
+            neighbours[b].append(a)
+
+
+        def dfs()
+
         
