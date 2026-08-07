56ff1e50df3119df40e27c74c9fb2ce9148c8cad
pratik376 <babariyapratik208@gmail.com>
2026-08-07 16:40:54 -0700

    Add new DSA practice files

---
+++ Graphs/2. DFS on Graphs/LC 1059 — All Paths from Source Lead to Destination.py
@@ -0,0 +1,41 @@
+from typing import List
+from collections import defaultdict
+class Solution:
+    def leadsToDestination(
+        self,
+        n: int,
+        edges: List[List[int]],
+        source: int,
+        destination: int
+    ) -> bool:
+
+
+        graph= defaultdict(list)
+
+        for a,b in edges:
+
+            graph[a].append(b)
+
+        stack=[source]
+        visited= set()
+
+
+        while stack:
+
+            node= stack.pop()
+
+            if not node==destination and not graph[node]:
+                return False
+
+
+            for nei in graph[node]:
+
+                if nei in visited:
+                    return False
+                else:
+                    visited.add(nei)
+                    stack.append(nei)
+
+        return True
+
+    
