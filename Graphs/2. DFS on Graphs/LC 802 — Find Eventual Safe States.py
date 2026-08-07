232e19175455a81a2c16555af80e9e587efe9815
pratik376 <babariyapratik208@gmail.com>
2026-08-06 18:33:49 -0700

    Add new DSA practice files

--- Graphs/2. DFS on Graphs/LC 802 — Find Eventual Safe States.py
+++ Graphs/2. DFS on Graphs/LC 802 — Find Eventual Safe States.py
@@ -1,3 +1,29 @@
+from typing import List
+
+
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+
+        n= len(graph)
+        safe= {}
+
+        def dfs(i):
+
+            if i in safe:
+                return safe[i]
+            
+
+
+
+
+
+
+
+        res=[]
+
+        for i in len(n):
+
+            if dfs(i):
+                res.append(i)
+        return res
         
