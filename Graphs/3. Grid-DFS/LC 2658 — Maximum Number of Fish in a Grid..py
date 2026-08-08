05d200640fae058b731862d142e3223e5f9af943
pratik376 <babariyapratik208@gmail.com>
2026-08-08 16:48:31 -0700

    Add new DSA practice files

--- Graphs/3. Grid-DFS/LC 2658 — Maximum Number of Fish in a Grid..py
+++ Graphs/3. Grid-DFS/LC 2658 — Maximum Number of Fish in a Grid..py
@@ -3,4 +3,44 @@ from typing import List
 
 class Solution:
     def findMaxFish(self, grid: List[List[int]]) -> int:
+
+
+        ROWS, COLUMS= len(grid), len(grid[0])
+        directions= [(1,0),(-1,0),(0,1),(0,-1)]
+        visited= set()
+
+        answer=0
+
+        def dfs(i,j):
+
+            fish=0
+
+            stack=[(i,j)]
+
+            if (i,j) not in visited and grid[i][j]>=1:
+                fish+=1
+
+            visited.add((i,j))
+
+            for R,C in directions:
+
+                nR,nC = R+i, C+j
+
+                if nR < 0 or nC< 0 or nR>=ROWS or nC>=COLUMS or (nR,nC) in visited or grid[nR][nC] ==0:
+                    continue
+
+                visited.add((nR,nC))
+                stack.append((nR,nC))
+                fish+=1
+
+            return fish
         
+
+        for i in range(ROWS):
+            for j in range(COLUMS):
+
+                if grid[i][j]>=1 and (i,j) not in visited:
+
+                    answer= max(answer,dfs(i,j))
+
+        return answer
