ad49b94de4d2bd0cd5e6ae37d2beaacf17171fd4
pratik376 <babariyapratik208@gmail.com>
2026-08-09 16:56:11 -0700

    Add new DSA practice files

--- Graphs/3. Grid-DFS/LC 1254 — Number of Closed Islands..py
+++ Graphs/3. Grid-DFS/LC 1254 — Number of Closed Islands..py
@@ -1,3 +1,50 @@
+from typing import List
+
+
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        
+        ROWS, COLUMNS = len(grid),len(grid[0])
+        visited= set()
+        closed_island=0
+
+        directions = [(0,1),(0,-1),(-1,0),(1,0)]
+
+        def dfs(i,j):
+
+            stack=[(i,j)]
+            visited.add((i,j))
+
+
+            while stack:
+                i,j = stack.pop()
+                
+
+
+
+
+        for i in range(ROWS):
+            for j in range(COLUMNS):
+
+                if grid[i][j]==0 and (i,j) not in visited:
+
+                    if dfs(i,j):
+                        closed_island+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
