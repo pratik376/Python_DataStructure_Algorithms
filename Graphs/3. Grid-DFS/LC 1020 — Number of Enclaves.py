3c0c1ad8e9a4ec0340130bf2aa2f8eb3f1f2da3d
pratik376 <babariyapratik208@gmail.com>
2026-08-10 16:57:41 -0700

    Add new DSA practice files

--- Graphs/3. Grid-DFS/LC 1020 — Number of Enclaves.py
+++ Graphs/3. Grid-DFS/LC 1020 — Number of Enclaves.py
@@ -33,6 +33,9 @@ class Solution:
                     if nR < 0 or nC< 0 or nR==ROWS or nC==COLS or (nR,nC) in visited or grid[nR][nC]==0:
                         continue
 
+                    if (nR==0 or nR==ROWS-1) or (nC==0 or nC==COLS-1):
+                        isBoundry=True
+
                     if ( 0<nR and nR<ROWS)  and ( 0<nC and nC < COLS):
                         answer+=1
                     visited.add((nR,nC))
