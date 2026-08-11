3e6819bbab355e89dfc5f717972c84ae2bb215c0
pratik376 <babariyapratik208@gmail.com>
2026-08-10 17:43:37 -0700

    Add new DSA practice files

--- Graphs/3. Grid-DFS/LC 1020 — Number of Enclaves.py
+++ Graphs/3. Grid-DFS/LC 1020 — Number of Enclaves.py
@@ -124,7 +124,7 @@ class Solution:
 
                     if nR < 0 or nC< 0 or nR==ROWS or nC==COLS or (nR,nC) in visited or grid[nR][nC]==0:
                         continue
-                    
+           
                     visited.add((nR,nC))
                     stack.append((nR,nC))
 
