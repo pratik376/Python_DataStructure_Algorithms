ab6c861cfeffb56c6898191c4691c34402e21d5d
pratik376 <babariyapratik208@gmail.com>
2026-08-10 17:40:50 -0700

    Add new DSA practice files

--- Graphs/3. Grid-DFS/LC 1020 — Number of Enclaves.py
+++ Graphs/3. Grid-DFS/LC 1020 — Number of Enclaves.py
@@ -88,3 +88,53 @@ class Solution:
                     borderLand += dfs(r, c)
 
         return land - borderLand
+
+# approved solution of original 
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+
+        ROWS, COLS= len(grid), len(grid[0])
+        directions= [(1,0),(-1,0),(0,1),(0,-1)]
+
+        visited= set()
+        final_answer=0
+        
+
+        def dfs(i,j):
+            answer=0
+            stack=[(i,j)]
+            visited.add((i,j))
+            isBoundry=0
+            count=0
+
+           
+
+            while stack:
+                i,j =stack.pop()
+
+                count+=1 # of current
+
+
+                for r,c in directions:
+
+                    nR,nC= r+i, c+j
+
+                    if nR < 0 or nC< 0 or nR==ROWS or nC==COLS or (nR,nC) in visited or grid[nR][nC]==0:
+                        continue
+
+                    if (nR==0 or nR==ROWS-1) or (nC==0 or nC==COLS-1):
+                        isBoundry=True
+
+                    if ( 0<nR and nR<ROWS)  and ( 0<nC and nC < COLS):
+                        answer+=1
+                    visited.add((nR,nC))
+                    stack.append((nR,nC))
+
+            return 0 if isBoundry else answer
+    
+        for i in range(ROWS):
+            for j in range(COLS):
+
+                if grid[i][j]==1 and not (i,j) in visited:
+                    final_answer+=dfs(i,j)
+        return final_answer
