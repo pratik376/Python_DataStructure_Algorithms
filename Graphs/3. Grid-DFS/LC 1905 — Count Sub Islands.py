7c758f7a86ad7cb1f9a9f154809b853bb100b027
pratik376 <babariyapratik208@gmail.com>
2026-08-10 18:22:33 -0700

    Add new DSA practice files

--- Graphs/3. Grid-DFS/LC 1905 — Count Sub Islands.py
+++ Graphs/3. Grid-DFS/LC 1905 — Count Sub Islands.py
@@ -8,4 +8,43 @@ class Solution:
         
         visited = set()
         directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+
+        def dfs(i,j):
+
+            stack=[(i,j)]
+            visited.add((i,j))
+            isSubIsland=True
+
+            while stack:
+
+                i,j=stack.pop()
+
+                if not grid1[i][j]:
+                    isSubIsland=False
+
+
+                for r,c in directions:
+
+                    nR, nC= r+i, c+j
+
+                    if nR < 0 or nC< 0 or nR==ROWS2 or nC==COLS2 or (nR,nC) in visited or grid2[nR][nC]==0:
+                        continue
+
+                    visited.add((i,j))
+                    stack.append((i,j))
+
+            return isSubIsland
+
+
+        sub_island=0
+        for i in range(ROWS2):
+            for j in range(COLS2):
+
+                if grid2[i][j]== 1 and (i,j) not in visited:
+
+                    if dfs(i,j):
+                        sub_island +=1
+
+        return sub_island
         
