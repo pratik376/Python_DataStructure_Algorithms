3e318364228a54065a16a79e64b274701ee9de83
pratik376 <babariyapratik208@gmail.com>
2026-08-10 19:45:02 -0700

    Add new DSA practice files

--- Graphs/3. Grid-DFS/LC 694 — Number of Distinct Islands.py
+++ Graphs/3. Grid-DFS/LC 694 — Number of Distinct Islands.py
@@ -5,48 +5,45 @@ class Solution:
 
         ROWS, COLS = len(grid), len(grid[0])
 
-        directions= [(1,0),(-1,0),(0,1),(0,-1)]
-        visited= set()
-        shapes=set
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        visited = set()
+        shapes = set()
 
+        def dfs(i, j):
 
+            startR, startC = i, j
+            shape = []
 
-        def dfs(i,j):
-
-            statR,startC=i,j
-            shape=[]
-
-            stack=[(i,j)]
+            stack = [(i,j)]
             visited.add((i,j))
 
             while stack:
 
-                i,j = stack.pop()
+                i, j = stack.pop()
 
-                shapes.append((i-statR,j-startC))
+                shape.append((i - startR, j - startC))
 
-                for r,c in directions:
-                    nR,nC= r+ i, c +j
+                for r, c in directions:
 
-                    if nR < 0 or nC< 0 or nR==ROWS or nC==COLS or (nR,nC) in visited or grid[nR][nC]==0:
+                    nR, nC = i + r, j + c
+
+                    if (
+                        nR < 0 or nC < 0 or
+                        nR >= ROWS or nC >= COLS or
+                        (nR,nC) in visited or
+                        grid[nR][nC] == 0
+                    ):
                         continue
+
                     visited.add((nR,nC))
                     stack.append((nR,nC))
 
             return tuple(sorted(shape))
-                
+
         for i in range(ROWS):
             for j in range(COLS):
 
-                if grid[i][j]== 1 and (i,j) not in visited:
+                if grid[i][j] == 1 and (i,j) not in visited:
                     shapes.add(dfs(i,j))
 
         return len(shapes)
-
-
-
-
-
-
-
-        
