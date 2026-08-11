f34f38bbddeca19f3644e162f02e3a3dca0a8a06
pratik376 <babariyapratik208@gmail.com>
2026-08-10 19:36:19 -0700

    Add new DSA practice files

---
+++ Graphs/3. Grid-DFS/LC 694 — Number of Distinct Islands.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+
+        ROWS, COLS = len(grid), len(grid[0])
+
+        directions= [(1,0),(-1,0),(0,1),(0,-1)]
+        visited= set()
+        shapes=set()
+
+
+
+        def dfs(i,j):
+
+            statR,startC=i,j
+
+            stack=[(i,j)]
+            visited.add((i,j))
+
+            
+
+
+
+        for i in range(ROWS):
+            for j in range(COLS):
+
+                if grid[i][j]== 1 and (i,j) not in visited:
+                    dfs(i,j)
+
+        return 
+
+
+
+
+
+
+
+        
