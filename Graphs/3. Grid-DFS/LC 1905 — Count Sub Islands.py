8666449f4300b49aa7cd20a1fdc84f3f8f871012
pratik376 <babariyapratik208@gmail.com>
2026-08-10 17:51:25 -0700

    Add new DSA practice files

--- Graphs/3. Grid-DFS/LC 1905 — Count Sub Islands.py
+++ Graphs/3. Grid-DFS/LC 1905 — Count Sub Islands.py
@@ -1,3 +1,11 @@
+from typing import List
+
+
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        ROWS1, COLS1 = len(grid1), len(grid1[0])
+        ROWS2, COLS2 = len(grid2), len(grid2[0])
+        
+        visited = set()
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
         
