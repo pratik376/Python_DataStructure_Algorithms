a1791e38481018eae4d506c46e3a7edb8e3cf76c
pratik376 <babariyapratik208@gmail.com>
2026-08-12 16:50:36 -0700

    Add new DSA practice files

--- Graphs/4. BFS/LC 752  — Open the Lock.py
+++ Graphs/4. BFS/LC 752  — Open the Lock.py
@@ -1,6 +1,51 @@
 from typing import List
-
+from collections import deque
 
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
+
+        
+
+        if target in deadends or "0000" in deadends:
+            return -1
+
+        visited= set(deadends)
+
+        q=deque()
+        q.append("1000")
+
+        visited.add(["1000",0]) # lock value
+
+        def children(lock):
+
+            res=[]
+
+            for i in range(4):
+
+                digit=str(int(lock[i]) +1 %10 )
+                res.append(lock[:i]+digit+lock[i+1:])
+                digit = str((int(lock[i])-1 +10)%10)
+                res.append(lock[:i]+digit+lock[i+1:])
+
+                return res
+                
+        while q:
+
+            lock, leval= q.popleft()
+
+            if lock==target:
+                return leval
+
+            for child in children(lock):
+
+                if child not in visited:
+                    visited.add(child)
+                    q.append([child,leval+1])
+
+        return -1
+
+
+
+
+
         
