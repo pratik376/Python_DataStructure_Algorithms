783f72ba31df8c0144a284620d0f1531b2cb4739
pratik376 <babariyapratik208@gmail.com>
2026-08-12 18:51:41 -0700

    Add new DSA practice files

--- Graphs/4. BFS/LC 909 — Snakes and Ladders.py
+++ Graphs/4. BFS/LC 909 — Snakes and Ladders.py
@@ -1,6 +1,56 @@
 from typing import List
-
+from collections import deque
 
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
+
+        n= len(board)
+        board.reverse()
+        q=deque([1,0]) # squre and steps
+        visited= set()
+
+        def intToPosition(sqare):
+            r= (sqare-1)// n
+            c=(sqare-1) % n
+
+            if r %2 :
+                c=len -1 -c
+
+            return [r,c]
+
+
+        while q:
+
+            sqare, step =q.popleft()
+
+            if sqare == n **2:
+                return step
+            for i in range(1,7):
+
+                nextSqaure=sqare+i
+                r,c = intToPosition(nextSqaure)
+
+                if board[r][c] != -1:
+                    nextSqaure=board[r][c]
+
+                if nextSqaure not in visited:
+                    q.append([nextSqaure, step+1])
+                    visited.add(nextSqaure)
+
+        return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
