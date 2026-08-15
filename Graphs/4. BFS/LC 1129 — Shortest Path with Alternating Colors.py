c65eea484480371ccab19f08bbae51562a9c55ab
pratik376 <babariyapratik208@gmail.com>
2026-08-14 17:57:06 -0700

    Add new DSA practice files

--- Graphs/4. BFS/LC 1129 — Shortest Path with Alternating Colors.py
+++ Graphs/4. BFS/LC 1129 — Shortest Path with Alternating Colors.py
@@ -1,4 +1,25 @@
+from typing import List
+from collections import defaultdict, deque
+
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+
+        blue=defaultdict(list)
+        red=defaultdict(list)
+
+
+        for  src,dst in redEdges:
+            blue[src].append(dst)
+
+        for src, dst in blueEdges:
+            red[src].append(dst)
+
+        q= deque()
+        visited= set()
+
+        answer=[-1] * n
+
+        
+
         
 
