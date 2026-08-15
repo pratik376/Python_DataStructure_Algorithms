a2559c2c1e51cab79a607d25b0fe0682670c728e
pratik376 <babariyapratik208@gmail.com>
2026-08-14 19:19:58 -0700

    Add new DSA practice files

--- Graphs/4. BFS/LC 815  — Bus Routes.py
+++ Graphs/4. BFS/LC 815  — Bus Routes.py
@@ -1,6 +1,20 @@
 from typing import List
 
+from collections import deque, defaultdict
+
 
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+
+        map=defaultdict(list)
+
+        for i in range(len(routes)):
+
+            for j in routes[i]:
+
+                map[j].append(i)
+
+        q=deque()
+        visited=set()
+
         
