d95a1012389afeaedf5f7d448242bf72a0cc6259
pratik376 <babariyapratik208@gmail.com>
2026-08-15 17:47:19 -0700

    Add new DSA practice files

--- Graphs/4. BFS/LC 773 — Sliding Puzzle.py
+++ Graphs/4. BFS/LC 773 — Sliding Puzzle.py
@@ -3,3 +3,12 @@ from collections import deque
 
 class Solution:
     def slidingPuzzle(self, board: List[List[int]]) -> int:
+        adj = {
+            0: [1, 3],
+            1: [0, 2, 4],
+            2: [1, 5],
+            3: [0, 4],
+            4: [1, 3, 5],
+            5: [2, 4]
+        }
+
