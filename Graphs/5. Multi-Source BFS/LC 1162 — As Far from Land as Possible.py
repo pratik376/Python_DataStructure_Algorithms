7aadda27b3f569c6dd8e89e4a3a6833beb538c63
pratik376 <babariyapratik208@gmail.com>
2026-08-16 19:02:20 -0700

    feat: add BM25 ranking and code-aware tokenization

--- Graphs/5. Multi-Source BFS/LC 1162 — As Far from Land as Possible.py
+++ Graphs/5. Multi-Source BFS/LC 1162 — As Far from Land as Possible.py
@@ -5,18 +5,39 @@ class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
 
 
-
+        if not grid:
+            return -1
         visited= set()
         ROWS,COLS= len(grid), len(grid[0])
         directions= [(1,0),(-1,0),(0,1),(0,-1)]
 
+        q=deque()
 
+        answer=-1
 
+        while q:
+            i,j,dist =q.popleft()
+
+            answer=max(answer,dist)
+
+            for r,c in directions:
+
+                nR,nC= r+i, c+j
+
+                if nR < 0 or nC <0 or nR>=ROWS or nC>=COLS or (nR,nC) in visited:
+                    continue
+
+                visited.add((nR,nC))
+                q.append((nR,nC,dist+1))
 
         for i in range(ROWS):
             for j in range(COLS):
 
-                if grid[i][j]==
+                if grid[i][j]==1:
+                    q.append((i,j,0))
+                    visited.add((i,j))
+
+        return answer
 
 
 
