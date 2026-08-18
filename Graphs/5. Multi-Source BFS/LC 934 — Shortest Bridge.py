521eebe62b1945287b3798d320859b3501e3faee
pratik376 <babariyapratik208@gmail.com>
2026-08-17 18:03:03 -0700

    Add new DSA practice files

--- Graphs/5. Multi-Source BFS/LC 934 — Shortest Bridge.py
+++ Graphs/5. Multi-Source BFS/LC 934 — Shortest Bridge.py
@@ -46,6 +46,16 @@ class Solution:
 
             i, j, dist= q.popleft()
 
-            
+            if (i,j) not in visited and grid[i][j]==1:
+                return dist
+
+            for r,c in directions:
+                nR,nC= i+r, j+c
+
+                if nR < 0 or nC <0 or nR>=N or nC>=N or (nR,nC) in visited or grid[nR][nC]==0:
+                    continue
+
+
+
             
         
