4ffb111071647d98e88dc5fc45c9f55e5ff0471b
pratik376 <babariyapratik208@gmail.com>
2026-08-19 16:32:25 -0700

    Add new DSA practice files

--- Graphs/6. Connected Components/LC 547 — Number of Provinces.py
+++ Graphs/6. Connected Components/LC 547 — Number of Provinces.py
@@ -11,9 +11,6 @@ class Solution:
         for i in range(N):
             for j in range(N):
 
-                if i == j:
-                    continue
-
                 if isConnected[i][j] == 1:
                     adjency[i].append(j)
 
@@ -37,11 +34,46 @@ class Solution:
                         stack.append(nei)
                         visited.add(nei)
 
-        for key in adjency.keys():
+        for key in adjency.keys(): 
 
             if not key in visited:
                 dfs(key)
                 answer+=1
 
         return answer
+
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+
+        N= len(isConnected)
+        visited=set()
+        answer=0
+
+        def dfs(city):
+
+            stack=[city]
+            visited.add(city)
+
+            while stack:
+                city=stack.pop()
+            
+
+                for nei in range(N):
+
+                    if nei not in visited and isConnected[city][nei]==1:
+                        visited.add(nei)
+                        stack.append(nei)
+                    
+        for city in range(N):
+
+            if city not in visited:
+                dfs(city)
+                answer+=1
+
+        return answer
+
+
+
 
