e273aa8eaa18dba455cdf206e33c41de7ba50e54
pratik376 <babariyapratik208@gmail.com>
2026-08-19 18:43:05 -0700

    Add new DSA practice files

--- Graphs/6. Connected Components/LC 399 — Evaluate Division.py
+++ Graphs/6. Connected Components/LC 399 — Evaluate Division.py
@@ -1,6 +1,48 @@
 from typing import List
-
+from collections import defaultdict,deque
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+
+        adj=defaultdict(list) # a-> [b, a/b]
+
+        for i, eq in enumerate(equations):
+
+            a,b= eq
+
+            adj[a].append([b,values[i] ])
+            adj[b].append([a, 1/values[i]])
+
+
+        visited=set()
+
+        def bfs(src, dest):
+
+            if src not in adj or dest not in adj:
+                return -1
+
+            q=deque((src, adj[src]))
+
+            visited.add(src)
+            result=1
+
+            while q:
+                node, val =q.popleft()
+
+                result*=val
+
+                if node==dest:
+                    return result
+
+                for nei in adj[src]:
+
+                    if nei not in visited:
+                        visited.add(nei)
+                        q.append((nei, adj[nei]))
+
+        answer=[]
+        for qx,qy in queries:
+            answer.append(bfs(qx,qy))
+
+
         
