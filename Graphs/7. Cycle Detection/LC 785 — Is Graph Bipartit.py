71ec00e34e4a43a5ee02454f93c8eb5a76bbc7da
pratik376 <babariyapratik208@gmail.com>
2026-08-20 20:03:29 -0700

    Add new DSA practice files

--- Graphs/7. Cycle Detection/LC 785 — Is Graph Bipartit.py
+++ Graphs/7. Cycle Detection/LC 785 — Is Graph Bipartit.py
@@ -5,21 +5,32 @@ class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
 
         visited=set()
-       
+        status={}
 
 
-        stack=[(0,"A")]
+        stack=[(0)]
+        status[0]="A"
 
         visited.add(0)
 
         while stack:
 
-            node, status =stack.pop()
+            node=stack.pop()
 
             for nei in graph[node]:
 
                 if nei not in visited:
-                    
+
+                    if status=="A":
+                        status[nei]="B"
+                    else:
+                        status[nei]="A"
+
+                elif status[node]==
+                      
+
+                
+
 
         
         
