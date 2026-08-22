6495395219f9d6f9e77449855d80265d7aa6f3c9
pratik376 <babariyapratik208@gmail.com>
2026-08-22 13:45:38 -0700

    Changed method name

--- Graphs/8. Topological Sorting/LC 2115 — Find All Possible Recipes from Given Supplies.py
+++ Graphs/8. Topological Sorting/LC 2115 — Find All Possible Recipes from Given Supplies.py
@@ -1,7 +1,12 @@
 from typing import List
-
+from collections import defaultdict, deque
 
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+
+        q=deque()
+
+        adj= defaultdict(list)
         
+
         
