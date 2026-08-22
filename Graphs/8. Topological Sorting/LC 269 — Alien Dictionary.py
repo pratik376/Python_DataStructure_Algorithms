abb6a19bdb37ca0da4e8022903970bf66aa71724
pratik376 <babariyapratik208@gmail.com>
2026-08-22 15:47:51 -0700

    Changed method name

--- Graphs/8. Topological Sorting/LC 269 — Alien Dictionary.py
+++ Graphs/8. Topological Sorting/LC 269 — Alien Dictionary.py
@@ -2,4 +2,12 @@ from typing import List
 
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        pass
+
+        adj= {c: set() for word in words for c in word}
+
+        for i in range(len(words)-1):
+
+            w1,w2 = words[i],words[i+1]
+
+            
+        
