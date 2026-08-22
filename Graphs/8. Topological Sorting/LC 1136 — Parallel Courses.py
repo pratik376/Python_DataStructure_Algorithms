c48ac34aac98f75f05295d2805e72f604c9d37d1
pratik376 <babariyapratik208@gmail.com>
2026-08-21 17:16:28 -0700

    Describe what changed

--- Graphs/8. Topological Sorting/LC 1136 — Parallel Courses.py
+++ Graphs/8. Topological Sorting/LC 1136 — Parallel Courses.py
@@ -1,5 +1,44 @@
 from typing import List
+from collections import defaultdict, deque
 
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
-        pass
+
+        q=deque()
+        adj= defaultdict(list)
+        degree = defaultdict(int)
+
+
+        for a,b in relations:
+
+            adj[a].append(b)
+            degree[b]+=1
+
+        answer=[]
+
+        min_sem=0
+
+        def bfs(q):
+
+            while q:
+
+               course, semester =q.popleft()
+               answer.append(course)
+               max_sem=max(max_sem,semester)
+
+               for nei in adj[course]:
+
+                   degree[nei]-=1
+
+                   if degree[nei]==0:
+
+                       q.append((nei, semester+1))
+                 
+        for i in range(n):
+            if degree[i]==0:
+                q.append((i,1))
+
+        bfs(q)
+
+        return min_sem if len(answer)==n else -1
+        
