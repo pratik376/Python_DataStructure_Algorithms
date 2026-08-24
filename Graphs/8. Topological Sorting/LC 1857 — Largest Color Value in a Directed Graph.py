d944adc7d8e594591a66f88400075dbb91abc87b
pratik376 <babariyapratik208@gmail.com>
2026-08-23 23:51:14 -0700

    Describe what changed

--- Graphs/8. Topological Sorting/LC 1857 — Largest Color Value in a Directed Graph.py
+++ Graphs/8. Topological Sorting/LC 1857 — Largest Color Value in a Directed Graph.py
@@ -140,3 +140,63 @@ class Solution:
                 return -1
 
         return answer
+
+
+
+# correct / neetcode solution
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+
+        adj = defaultdict(list)
+
+        for src, dst in edges:
+            adj[src].append(dst)
+
+        # Return max frequency of color
+        def dfs(node):
+
+            if node in path:
+                return float("inf")
+
+            if node in visit:
+                return 0
+
+            visit.add(node)
+            path.add(node)
+
+            colorIndex = ord(colors[node]) - ord('a')
+            count[node][colorIndex] = 1
+
+            for nei in adj[node]:
+
+                if dfs(nei) == float("inf"):
+                    return float("inf")
+
+                for c in range(26):
+
+                    count[node][c] = max(
+                        count[node][c],
+                        (1 if c == colorIndex else 0) + count[nei][c]
+                    )
+
+            path.remove(node)
+
+            return max(count[node])
+
+        n = len(colors)
+        res = 0
+
+        visit = set()
+        path = set()
+
+        # count[node][color] = max frequency of color
+        count = [[0] * 26 for _ in range(n)]
+
+        for i in range(n):
+            res = max(dfs(i), res)
+
+        return -1 if res == float("inf") else res
