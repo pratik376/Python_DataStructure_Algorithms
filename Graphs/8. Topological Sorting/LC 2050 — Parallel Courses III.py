020a02af2adaf30ca74cb824e3cd5ca02e1a1104
pratik376 <babariyapratik208@gmail.com>
2026-08-23 17:23:30 -0700

    Describe what changed

--- Graphs/8. Topological Sorting/LC 2050 — Parallel Courses III.py
+++ Graphs/8. Topological Sorting/LC 2050 — Parallel Courses III.py
@@ -56,4 +56,52 @@ class Solution:
 
         bfs(q)
 
-        return answer
+        return time
+    
+from typing import List
+from collections import defaultdict, deque
+
+class Solution:
+    def minimumTime(
+        self,
+        n: int,
+        relations: List[List[int]],
+        time: List[int]
+    ) -> int:
+
+        adj = defaultdict(list)
+        indegree = [0] * (n + 1)
+
+        for u, v in relations:
+            adj[u].append(v)
+            indegree[v] += 1
+
+        q = deque()
+
+        # finish[i] = earliest time course i finishes
+        finish = [0] * (n + 1)
+
+        # Courses are numbered 1...n
+        for course in range(1, n + 1):
+
+            if indegree[course] == 0:
+                finish[course] = time[course - 1]
+                q.append(course)
+
+        while q:
+
+            course = q.popleft()
+
+            for nei in adj[course]:
+
+                finish[nei] = max(
+                    finish[nei],
+                    finish[course] + time[nei - 1]
+                )
+
+                indegree[nei] -= 1
+
+                if indegree[nei] == 0:
+                    q.append(nei)
+
+        return max(finish)
