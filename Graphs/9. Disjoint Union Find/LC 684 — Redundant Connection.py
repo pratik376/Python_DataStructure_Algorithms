1bc587ce33cc0f59310d6ace234e6bcd218a86d2
pratik376 <babariyapratik208@gmail.com>
2026-08-26 16:27:09 -0700

    Changed method name

--- Graphs/9. Disjoint Union Find/LC 684 — Redundant Connection.py
+++ Graphs/9. Disjoint Union Find/LC 684 — Redundant Connection.py
@@ -6,4 +6,40 @@ class Solution:
 
         parent= [i for i in range(len(edges)+1)]
         rank= [1] * (len(edges)+1)
+
+        def find(n):
+
+            p=parent[n]
+
+            while p !=parent[p]:
+                parent[p]=parent[parent[p]]
+                p=parent[p]
+            return p
+
+        def union(n1,n2):
+            p1,p2 = find(n1), find(n2)
+
+            if p1==p2:  # cycle
+                return False
+
+            if rank[p1]> rank[p2]:
+
+                parent[p2]=p1
+                rank[p1]+=rank[p2]
+
+            else:
+
+                parent[p1]=p2
+                rank[p2]+= rank[p1]
+
+            return True
+
+        for a,b in edges:
+
+            if not union(a,b):
+                return [a,b]
+            
+
+
+            
         
---
+++ Graphs/9. Disjoint Union Find/LC 737 — Sentence Similarity II.py
@@ -0,0 +1,54 @@
+from typing import List
+
+class Solution:
+    def areSentencesSimilarTwo( self,sentence1: List[str],sentence2: List[str],similarPairs: List[List[str]]) -> bool:
+
+
+        if len(sentence1) != len(sentence2):
+            return False
+
+        parents={}
+        rank={}
+
+        for pair in similarPairs:
+
+            for p in pair:
+                parents[p] = p
+                rank[p]=1
+
+        def find(n):
+
+            p= parents[n]
+
+            while p !=parents[p]:
+                parents[p]=parents[parents[p]]
+                p=parents[p]
+            return p
+
+        def union(a1,a2):
+            p1,p2= find(a1),find(a2)
+
+            if p1==p2:
+                return True
+
+            if rank[p1]> rank[p2]:
+
+                parents[p2]=parents[p1]
+                rank[p1]+=rank[p2]
+
+            else:
+                parents[p1]=parents[p2]
+                rank[p2]+= rank[p1]
+
+            return False
+
+        for a,b in similarPairs:
+            union(a,b)
+
+        for a1,a2 in zip(sentence1,sentence2):
+
+            if not union(a1,a2):
+                return False
+
+        return True
+            
