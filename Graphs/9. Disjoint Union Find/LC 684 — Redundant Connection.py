32213177095e0a178200a7f364fd77cbefcb868b
pratik376 <babariyapratik208@gmail.com>
2026-08-26 15:38:57 -0700

    Changed method name

--- Graphs/9. Disjoint Union Find/LC 684 — Redundant Connection.py
+++ Graphs/9. Disjoint Union Find/LC 684 — Redundant Connection.py
@@ -3,4 +3,7 @@ from typing import List
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+        parent= [i for i in range(len(edges)+1)]
+        rank= [1] * (len(edges)+1)
         
