215973e66b3e33aa038e19782fc3c77d806f5ad3
pratik376 <babariyapratik208@gmail.com>
2026-08-26 18:57:37 -0700

    Changed method name

--- Graphs/9. Disjoint Union Find/LC 305 — Number of Islands II.py
+++ Graphs/9. Disjoint Union Find/LC 305 — Number of Islands II.py
@@ -6,6 +6,40 @@ class Solution:
         parents= [i for i in range(m*n)]
         rank= [1] * (m *n)
         parents= [-1] * (m *n)
+
+        directions= [ (1,0),(-1,0),(0,1),(0,-1)]
+
+        isLand=0
+        answer=[]
+
+        def find(node):
+
+            while node != parents[node]:
+
+                node=parents[node]
+                parents[node]= parents[parents[node]]
+
+            return node
+
+        def union(a,b):
+
+            p1=find(a)
+            p2=find(b)
+
+            if p1==p2:
+                return True
+
+            if rank[p1]> rank[p2]:
+
+                parents[p2]=p1
+                rank[p1]+=rank[p2]
+
+            else:
+                parents[p1]=p2
+                rank[p2]+=rank[p1]
+
+            return False
         
+
 
 
