e3a2db77215ce7d603df7e8f4073650b4329bf7d
pratik376 <babariyapratik208@gmail.com>
2026-08-26 18:50:30 -0700

    Changed method name

--- Graphs/9. Disjoint Union Find/LC 305 — Number of Islands II.py
+++ Graphs/9. Disjoint Union Find/LC 305 — Number of Islands II.py
@@ -4,4 +4,6 @@ class Solution:
     def numIslands2( self,m: int,n: int,positions: List[List[int]] ) -> List[int]:
 
         parents= [i for i in range(m*n)]
+        rank= [1] * (m *n)
         
+
