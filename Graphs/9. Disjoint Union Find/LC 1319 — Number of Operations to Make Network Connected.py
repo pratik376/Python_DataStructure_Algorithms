45c63a0530181d2399c82164ad3eb5a1ba5c2f2d
pratik376 <babariyapratik208@gmail.com>
2026-08-28 14:46:51 -0700

    Changed method name

--- Graphs/9. Disjoint Union Find/LC 1319 — Number of Operations to Make Network Connected.py
+++ Graphs/9. Disjoint Union Find/LC 1319 — Number of Operations to Make Network Connected.py
@@ -5,10 +5,12 @@ class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
 
     
+        if len(connections) < n-1:
+            return -1
+        
+        parents= [i for i in range(len(n))]
 
-        parents= [i for i in range(len(connections))]
-
-        rank= [1] * len(connections)
+        rank= [1] * len(n)
 
 
         def find(n):
@@ -36,19 +38,75 @@ class Solution:
                 rank[p2]+=rank[p1]
             return True
 
-        cables=0
+        component= n
 
         for a,b in connections:
 
             if not union(a,b):
-                cables+=1
+                component-=1
 
 
-        return cables if cables + len(connections) == n else -1
+        return component-1
             
 
         
+from typing import List
 
+
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+
+        parent = [i for i in range(n)]
+        size = [1] * n
+
+        def find(node):
+
+            p = parent[node]
+
+            while p != parent[p]:
+                parent[p] = parent[parent[p]]
+                p = parent[p]
+
+            return p
+
+        def union(a, b):
+
+            p1 = find(a)
+            p2 = find(b)
+
+            if p1 == p2:
+                return False
+
+            if size[p1] > size[p2]:
+
+                parent[p2] = p1
+                size[p1] += size[p2]
+
+            else:
+
+                parent[p1] = p2
+                size[p2] += size[p1]
+
+            return True
+
+        extra_cables = 0
+        components = n
+
+        for a, b in connections:
+
+            if union(a, b):
+                components -= 1
+
+            else:
+                extra_cables += 1
+
+        cables_needed = components - 1
+
+        if extra_cables >= cables_needed:
+            return cables_needed
+
+        return -1
+    
         
 
         
