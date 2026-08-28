959466314b7154acca84b4c771f45fc232293079
pratik376 <babariyapratik208@gmail.com>
2026-08-28 15:11:26 -0700

    Changed method name

--- Graphs/9. Disjoint Union Find/LC 1579 — Remove Max Number of Edges to Keep Graph Fully Traversable.py
+++ Graphs/9. Disjoint Union Find/LC 1579 — Remove Max Number of Edges to Keep Graph Fully Traversable.py
@@ -1,3 +1,20 @@
+from typing import List
+
+
 class Solution:
     def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
+
+        parents= [i for i in range(n+1)]
+        rank= [1] * (n+1)
+
+        edges_storage={}
+
+
+        def find(n):
+
+            while n != parents[n]:
+                parents[n]= parents[parents[n]]   
+                n=parents[n]
+            return n
+
         
