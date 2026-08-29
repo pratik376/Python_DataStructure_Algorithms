23bbde64d0bddcd8065ddc5b22bfc9649381ca50
pratik376 <babariyapratik208@gmail.com>
2026-08-29 15:58:29 -0700

    Changed method name

--- Graphs/10. Tress in graphs/LC 2492 — Minimum Score of a Path Between Two Cities.py
+++ Graphs/10. Tress in graphs/LC 2492 — Minimum Score of a Path Between Two Cities.py
@@ -1,3 +1,35 @@
+from typing import List
+
+from collections import defaultdict,deque
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
+
+        edgeList= defaultdict(list)
+
+        q=deque()
+        q.append((1,float('inf')))
+        answer=float('inf')
+        visited=set()
+        visited.add(1)
+
+
+        for a,b, weight in roads:
+
+            edgeList[a].append((b,weight))
+            edgeList[b].append((a,weight))
+
+
+        def bfs():
+
+            while q:
+                node, w=q.popleft()
+
+                answer=min(answer,w)
+
+                for nei,weight in edgeList[node]:
+
+
+                
+
+
         
