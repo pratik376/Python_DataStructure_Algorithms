be332674358da78865a61a7892250411ac401651
pratik376 <babariyapratik208@gmail.com>
2026-08-31 21:33:17 -0700

    Changed method name

--- Graphs/10. Tress in graphs/LC 2467 — Most Profitable Path in a Tree.py
+++ Graphs/10. Tress in graphs/LC 2467 — Most Profitable Path in a Tree.py
@@ -10,18 +10,69 @@ class Solution:
             adj[a].append(b)
             adj[b].append(a)
 
-        visited= set()
+        bob_times={}
+
+        def dfs(src,prev,time):
+
+            if src==0:
+                bob_times[src]=time
+                return True
+
+            for nei in adj[src]:
+
+                if nei!= prev:
+                   if dfs(nei,src,time+1):
+                       bob_times[src]=time
+                       return True
+
+            return False
+
+        dfs(bob,-1,0)
+
+        q=deque([0,0,-1,amount[0]]) # (node, time, parent, total profit)
+
+        res=float("-inf")
+
+        while q:
+           node, time, parent, profit =q.popleft()
+
+           for nei in adj[node]:
+
+               if nei == parent:
+                   continue
+
+               nei_profit= amount[nei]
+               nei_time= time+1
+
+               if nei in bob_times:
+                   
+                 if nei_time > bob_times[nei]:
+                     nei_profit=0
+
+                 if nei_time == bob_times:
+                     nei_profit = nei_profit//2
+
+               q.append((nei,nei_time,node, nei_profit+profit))
+
+               if len(adj[nei])==1:
+                   res= max(res,nei_profit+profit)
+        return res
+                   
+                   
+
+
+            
+
+        
 
 
 
-        def dfs(start):
+      
+            
 
-            stack=[start]
-            time=0 
-            visited.add((start,time))
 
-            while stack:
 
-                node= stack.pop()
-                
+
+
+
         
