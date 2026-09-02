9f5b40c023d036817d5aba30e87238e803596253
pratik376 <babariyapratik208@gmail.com>
2026-09-01 18:50:45 -0700

    Changed method name

--- Graphs/11. Heap in Graphs/LC 743 — Network Delay Time.py
+++ Graphs/11. Heap in Graphs/LC 743 — Network Delay Time.py
@@ -5,7 +5,31 @@ class Solution:
 
 
         adj= defaultdict(list)
-        
+
+        for a,b, w in times:
+            adj[b].append((b,w))
+
+        answer=0
+        visited= set()
+
+        def dfs(node,time):
+
+            stack=[node]
+            visited.add(node)
+
+            while stack:
+                node, time =stack.pop()
+
+                answer= max(answer,time)
+
+                for nei, nei_time in adj[node]:
+
+                    if nei not in visited:
+                        visited.add(nei)
+                        stack.append((nei,nei_time ))
+
+            return answer
+
 
 
         
