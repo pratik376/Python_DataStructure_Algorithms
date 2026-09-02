1550035bca190cb91d1093e463457f4f070ed05c
pratik376 <babariyapratik208@gmail.com>
2026-09-02 16:32:29 -0700

    Changed method name

--- Graphs/11. Heap in Graphs/LC 1102 — Path With Maximum Minimum Value.py
+++ Graphs/11. Heap in Graphs/LC 1102 — Path With Maximum Minimum Value.py
@@ -1,6 +1,39 @@
 from typing import List
-
+import heapq
 
 class Solution:
     def maximumMinimumPath(self, grid: List[List[int]]) -> int:
-        pass
+
+    
+
+        maxheap= [(-grid[0][0],0,0)]
+
+        ROWS, COLS= len(grid), len(grid[0])
+        visited=set()
+        directions= [(1,0),(-1,0),(0,1),(0,-1)]
+
+        while maxheap:
+
+            val , r, c = heapq.heappop(maxheap)
+            val=-val
+
+            if r ==ROWS-1 and c==COLS-1:
+                return val
+
+            if (r,c) in visited:
+                continue
+
+            visited.add((r,c))
+
+            for nr,nc in directions:
+
+                R,C= nr+r, nc+c
+
+                if R< 0 or R==ROWS or C<0 or C==COLS or (R,C) in visited:
+                    continue
+                
+                
+                heapq.heappush(maxheap, (-min(val, grid[R][C]), R,C))
+
+
+        
