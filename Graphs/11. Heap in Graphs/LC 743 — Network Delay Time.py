fbd9eab9ee4f167695afb338c363a37f2c0d2b1c
pratik376 <babariyapratik208@gmail.com>
2026-09-01 18:47:49 -0700

    Changed method name

---
+++ Graphs/11. Heap in Graphs/LC 743 — Network Delay Time.py
@@ -0,0 +1,11 @@
+from typing import List
+from collections import defaultdict
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+
+
+        adj= defaultdict(list)
+        
+
+
+        
