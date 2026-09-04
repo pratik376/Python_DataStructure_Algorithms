584abd56687b237bd9778f3a597d472cbe8b6992
pratik376 <babariyapratik208@gmail.com>
2026-09-03 21:18:55 -0700

    Changed method name

--- Graphs/11. Heap in Graphs/LC 787 — Cheapest Flights Within K Stops.py
+++ Graphs/11. Heap in Graphs/LC 787 — Cheapest Flights Within K Stops.py
@@ -62,3 +62,36 @@ class Solution:
                 )
 
         return answer if answer != float("inf") else -1
+
+
+    from typing import List
+
+
+class Solution:
+    def findCheapestPrice(
+        self,
+        n: int,
+        flights: List[List[int]],
+        src: int,
+        dst: int,
+        k: int
+    ) -> int:
+
+        prices = [float("inf")] * n
+        prices[src] = 0
+
+        for i in range(k + 1):
+
+            tmpPrices = prices.copy()
+
+            for s, d, p in flights:
+
+                if prices[s] == float("inf"):
+                    continue
+
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+
+            prices = tmpPrices
+
+        return -1 if prices[dst] == float("inf") else prices[dst]
