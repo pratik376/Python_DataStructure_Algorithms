8d5db5a0187a6e52af237cbf3e31f34156ef9e13
pratik376 <babariyapratik208@gmail.com>
2026-09-10 16:25:29 -0700

    Changed method name

--- Graphs/12. Graph + State Space Search/LC 490 — The Maze.py
+++ Graphs/12. Graph + State Space Search/LC 490 — The Maze.py
@@ -6,7 +6,7 @@ class Solution:
 
 
 
-
+        ROWS, COLS= len(maze), len(maze[0])
         visited=set()
         directions=[(1,0),(-1,0),(0,1),(0,-1)]
 
@@ -24,7 +24,20 @@ class Solution:
             for R,C in directions:
 
                 nr,nc= r,c
-                
+
+                while (0<= nr+R <ROWS and 0<= nc+C <COLS and maze[nr+R][nc+C]==1):
+                    nr+=R
+                    nc+=C
+
+                if ((nr,nc)) not in visited and 0<= nr+R <ROWS and 0<= nc+C <COLS:
+                    visited.add((nr,nc))
+                    stack.append((nr,nc))
+
+        return False
+            
+
+    
+
 
 
 
