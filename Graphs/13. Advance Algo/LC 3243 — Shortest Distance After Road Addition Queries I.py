23cea346cd30413f3dd2218fd6cad6655c9fdf45
pratik376 <babariyapratik208@gmail.com>
2026-09-11 16:31:01 -0700

    Changed method name

--- Graphs/13. Advance Algo/LC 3243 — Shortest Distance After Road Addition Queries I.py
+++ Graphs/13. Advance Algo/LC 3243 — Shortest Distance After Road Addition Queries I.py
@@ -1,3 +1,43 @@
+from typing import List
+
+from collections import defaultdict,deque
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+
+        answer=[0] * len(queries)
+
+        adj=defaultdict(list)
+
+        for i in range(n-1):
+            adj[i].append(i+1)
+
+
+
+        def bfs():
+
+            visited= set()
+            q=deque((0,0)) # node, steps
+
+
+            while q:
+
+                node, cost =q.popleft()
+
+                if node==n-1:
+                    return cost
+
+                for  nei in adj[node]:
+
+                    if nei not in visited:
+                        q.append((nei,cost+1))
+                        visited.add((nei))
+
+        for index, val in enumerate(queries):
+            a,b= val
+            adj[a].append(b)
+
+            answer[index]=bfs()
+
+        return answer
+
         
