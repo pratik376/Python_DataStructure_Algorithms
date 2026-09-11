c12cbd165b78f3d167fd8c8204051ff3973c28e0
pratik376 <babariyapratik208@gmail.com>
2026-09-10 17:08:34 -0700

    Changed method name

--- Graphs/12. Graph + State Space Search/LC 1197 — Minimum Knight Moves.py
+++ Graphs/12. Graph + State Space Search/LC 1197 — Minimum Knight Moves.py
@@ -3,3 +3,28 @@ from collections import deque
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
         moves = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+
+
+        visited= set()
+        q=deque()
+        q.append((0,0,0)) # moves, x,y
+        visited.add((0,0))
+
+
+        while q:
+            cost, r,c=q.popleft()
+
+            if (r,c)==(x,y):
+                return cost
+
+
+            for R,C in moves:
+
+                nr,nc= r+R ,c+ C
+
+                if (nr,nc) not in visited:
+                    visited.add((nr,nc))
+                    q.append((cost+1, nr,nc))
+
+        return -1
+            
