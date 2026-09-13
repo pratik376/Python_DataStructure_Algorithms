3f50db8740dc5f6d9811948979c9a20ef38d0db7
pratik376 <babariyapratik208@gmail.com>
2026-09-13 15:57:45 -0700

    Changed method name

--- Graphs/13. Advance Algo/LC 2045 — Second Minimum Time to Reach Destination.py
+++ Graphs/13. Advance Algo/LC 2045 — Second Minimum Time to Reach Destination.py
@@ -1,6 +1,12 @@
 from typing import List
 
-
+from collections import defaultdict
 class Solution:
     def secondMinimum(self, n: int, edges: List[List[int]], time: int, change: int) -> int:
+        adj=defaultdict(list)
+
+        for v1, v2 in edges:
+            adj[v1].append(v2)
+            adj[v2].append(v1)
+
         
