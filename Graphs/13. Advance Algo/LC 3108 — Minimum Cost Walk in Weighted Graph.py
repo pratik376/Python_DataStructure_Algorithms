1a4562cbcc0139ca2a993bc646747ca39be12ea4
pratik376 <babariyapratik208@gmail.com>
2026-09-16 20:01:16 -0700

    Changed method name

--- Graphs/13. Advance Algo/LC 3108 — Minimum Cost Walk in Weighted Graph.py
+++ Graphs/13. Advance Algo/LC 3108 — Minimum Cost Walk in Weighted Graph.py
@@ -1,3 +1,57 @@
+from typing import List
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1] * n
+
+    def find(self, v1):
+        while v1 != self.par[v1]:
+            self.par[v1] = self.par[self.par[v1]]
+            v1 = self.par[v1]
+        return v1
+
+    def union(self, v1, v2):
+        p1, p2 = self.find(v1), self.find(v2)
+
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p2]
+        return True
+
+
 class Solution:
     def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
+
+        uf=UnionFind(n)
+
+        for u,v, w in edges:
+            uf.union(u,v)
+
+        component_cost={} # root ->cost
+
+        for u,v ,w in edges:
+
+            root=uf.find(u)
+
+            if root not in component_cost:
+                component_cost[root]=w
+            else:
+                component_cost[root] &=w
+
+        res=[]
+        
+
+
+
+
+
+         
+
         
