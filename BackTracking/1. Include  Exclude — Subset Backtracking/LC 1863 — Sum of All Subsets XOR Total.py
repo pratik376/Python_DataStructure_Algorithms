9ca8cec0b3a76dea78a5d60ffb6a36ea294f95f6
pratik376 <babariyapratik208@gmail.com>
2026-09-18 19:18:04 -0700

    Changed method name

--- BackTracking/1. Include  Exclude — Subset Backtracking/LC 1863 — Sum of All Subsets XOR Total.py
+++ BackTracking/1. Include  Exclude — Subset Backtracking/LC 1863 — Sum of All Subsets XOR Total.py
@@ -6,14 +6,16 @@ class Solution:
 
         def dfs(i,xor):
 
-            if i ==len(nums)-1:
+            if i ==len(nums):
                 return xor
 
             # add
             # what i was about to do xor+= xor ^ nums[i]
-            dfs(i+1, nums[i] ^ xor)
+            include=dfs(i+1, nums[i] ^ xor)
             # remove  xor-= xor ^ nums[i]
-            dfs(i+1, xor)
+            exclude=dfs(i+1, xor)
+
+            return include + exclude
 
         return dfs(0,0)
 
---
+++ BackTracking/1. Include  Exclude — Subset Backtracking/LC 2044 — Count Number of Maximum Bitwise-OR Subsets.py
@@ -0,0 +1,30 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: list[int]) -> int:
+
+
+        max_OR=0
+
+
+        for num in nums:
+
+            max_OR |= num
+
+        count=0
+
+        def dfs(i, Nor):
+
+            if i < len(nums) and Nor==max_OR:
+                count+=1
+                return
+
+            #add
+
+            dfs(i+1, Nor | nums[i])
+
+            #not add
+
+            dfs(i+1, Nor)
+
+        return count
+
+
