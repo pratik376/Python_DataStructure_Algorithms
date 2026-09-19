098485553c8a6d45fd8ff8f26b30e0d14d3f4487
pratik376 <babariyapratik208@gmail.com>
2026-09-18 19:28:39 -0700

    Changed method name

--- BackTracking/1. Include  Exclude — Subset Backtracking/LC 2044 — Count Number of Maximum Bitwise-OR Subsets.py
+++ BackTracking/1. Include  Exclude — Subset Backtracking/LC 2044 — Count Number of Maximum Bitwise-OR Subsets.py
@@ -1,33 +1,29 @@
 class Solution:
     def countMaxOrSubsets(self, nums: list[int]) -> int:
 
-
-        max_OR=0
-
+        max_OR = 0
 
         for num in nums:
 
             max_OR |= num
 
-        count=0
+        count = 0
 
         def dfs(i, Nor):
             nonlocal count
 
-            if i== len(nums):
+            if i == len(nums):
                 return
 
-            if i <= len(nums) and Nor==max_OR:
-                count+=1
+            if i < len(nums) and Nor == max_OR:
+                count += 1
 
-            #add
+            # add
 
-            dfs(i+1, Nor | nums[i])
+            dfs(i + 1, Nor | nums[i])
 
-            #not add
+            # not add
 
-            dfs(i+1, Nor)
+            dfs(i + 1, Nor)
 
         return count
-
-
