90e0123ef5e0ebd3944716dd676eb61557902010
pratik376 <babariyapratik208@gmail.com>
2026-09-18 19:03:09 -0700

    Changed method name

--- BackTracking/1. Include  Exclude — Subset Backtracking/LC 1863 — Sum of All Subsets XOR Total.py
+++ BackTracking/1. Include  Exclude — Subset Backtracking/LC 1863 — Sum of All Subsets XOR Total.py
@@ -1,3 +1,23 @@
 class Solution:
     def subsetXORSum(self, nums: list[int]) -> int:
+
+        res=[]
+        subset=[]
+
+        def dfs(i,xor):
+
+            if i >len(nums):
+                return xor
+
+
+            # add
+            
+        
+            dfs(i+1, nums[i] * xor)
+
+            # remove
+            dfs(i+1, xor)
+
+        return dfs(0,0)
+
         
