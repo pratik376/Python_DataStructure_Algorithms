7f507caf67d9cd2c21d4f931f6eb938d66437b23
pratik376 <babariyapratik208@gmail.com>
2026-09-18 17:37:08 -0700

    Changed method name

---
+++ BackTracking/New folder/template.py
@@ -0,0 +1,14 @@
+def backtrack(params):
+    if base_case_condition:
+        results.append(copy_of_solution)
+        return
+
+    for choice in choices:
+        if violates_constraints:
+            continue
+
+        make_choice
+
+        backtrack(updated_params)
+
+        undo_choice  # backtracking step
