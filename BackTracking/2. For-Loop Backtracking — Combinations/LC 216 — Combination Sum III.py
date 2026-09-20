efdf4b1b0b0f4ba9e995f2281ee006b49701a444
pratik376 <babariyapratik208@gmail.com>
2026-09-20 16:22:23 -0700

    Changed method name

--- BackTracking/2. For-Loop Backtracking — Combinations/LC 216 — Combination Sum III.py
+++ BackTracking/2. For-Loop Backtracking — Combinations/LC 216 — Combination Sum III.py
@@ -1,3 +1,27 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> list[list[int]]:
+
+
+        res=[]
+
+        def dfs(start, currentSum, comb):
+
+            if len(comb)>k:
+                return
+            if currentSum==n and len(comb)==k:
+                res.append(comb.copy())
+                return
+
+            for i in range(start, 10):
+
+                comb.append(i)
+                dfs(i+1, sum +i, comb)
+                comb.pop()
+
+        dfs(1,0,[])
+        return res
+
+
+            
+
         
