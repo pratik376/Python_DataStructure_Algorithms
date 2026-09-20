a6837467e46f69492e552f3796bd4c5b30ffab20
pratik376 <babariyapratik208@gmail.com>
2026-09-19 17:59:20 -0700

    Changed method name

--- BackTracking/1. Include  Exclude — Subset Backtracking/LC 2597 — The Number of Beautiful Subsets.py
+++ BackTracking/1. Include  Exclude — Subset Backtracking/LC 2597 — The Number of Beautiful Subsets.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from collections import defaultdict
 
 class Solution:
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
@@ -15,23 +15,47 @@ class Solution:
 
             # keep
 
-            flag=0
+            can_include=True
             for element in subset:
 
-                if abs(element - nums[i])!=k:
-                    flag=1
+                if abs(element - nums[i])==k:
+                    can_include=False
+                    break
 
-            if not flag:
+            if can_include:
                 count+=1
-
-            subset.append(nums[i])
-            dfs(i+1, subset)
-
-            # remove
-            subset.pop()
+                subset.append(nums[i])
+                dfs(i+1, subset)
+                subset.pop()
             dfs(i+1,subset)
 
         dfs(0,subset)
+
+        return count
+
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+
+        count =0
+        freq=defaultdict(int)
+
+        def dfs(i):
+            nonlocal freq,count
+
+            if i>= len(nums):
+                return
+
+
+            x=nums[i]
+
+            if freq[x-k]==0 and freq[x+k]==0:
+                count+=1
+
+                freq[x]+=1
+                dfs(i+1)
+                freq[x]-=1
+
+            dfs(i+1)
 
         return count
 
