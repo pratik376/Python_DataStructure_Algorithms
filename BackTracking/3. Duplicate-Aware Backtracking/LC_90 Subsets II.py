d94005c8788919f62d266b3004dd4197d6ac8c61
pratik376 <babariyapratik208@gmail.com>
2026-09-21 16:19:55 -0700

    Changed method name

--- BackTracking/3. Duplicate-Aware Backtracking/LC_90 Subsets II.py
+++ BackTracking/3. Duplicate-Aware Backtracking/LC_90 Subsets II.py
@@ -22,5 +22,30 @@ class Solution:
         dfs(0,[])
         return res
 
+class Solution:
+    def subsetsWithDup(self, nums: list[int]) -> list[list[int]]:
+
+        nums.sort()
+        res=[]
+
+
+        def dfs(start, path):
+
+
+            for i in range(start, len(nums)):
+                res.append(path.copy())
+
+                if i > start and nums[i]==nums[i-1]:
+
+                    continue
+
+                path.append(nums[i])
+                dfs(i+1, path)
+                path.pop()
+
+        dfs(0,[])
+        return res
+
+
 
 
