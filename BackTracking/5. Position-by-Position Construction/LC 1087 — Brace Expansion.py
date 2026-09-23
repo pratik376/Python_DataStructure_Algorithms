82349a609e8df1b8614ec6414589febf95aff916
pratik376 <babariyapratik208@gmail.com>
2026-09-22 17:44:22 -0700

    Changed method name

---
+++ BackTracking/5. Position-by-Position Construction/LC 1087 — Brace Expansion.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def expand(self, s: str) -> List[str]:
+        pass
---
+++ BackTracking/5. Position-by-Position Construction/LC 17 — Letter Combinations of a Phone Number.py
@@ -0,0 +1,31 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+
+        n= len(digits)
+        res=[]
+        digitToChar = { "2": "abc",
+                "3": "def",
+                "4": "ghi",
+                "5": "jkl",
+                "6": "mno",
+                "7": "qprs",
+                "8": "tuv",
+                "9": "wxyz" }
+
+        def dfs(pos, path):
+
+            if len(path)==n:
+
+                res.append(path[:])
+                return
+            
+            for c in  digitToChar[digits[pos]]:
+                path.append(c)
+                dfs(pos+1,path)
+                path.pop()
+
+        if digits:
+            dfs(0,"")
+
+        return res
+        
