ccfa5126199e952208ffa17e055ce94895b2ab2e
pratik376 <babariyapratik208@gmail.com>
2026-09-22 18:19:52 -0700

    Changed method name

--- BackTracking/5. Position-by-Position Construction/LC 1087 — Brace Expansion.py
+++ BackTracking/5. Position-by-Position Construction/LC 1087 — Brace Expansion.py
@@ -3,4 +3,35 @@ from typing import List
 
 class Solution:
     def expand(self, s: str) -> List[str]:
-        pass
+
+        index=-1
+        groups=[]
+
+        i=0
+
+        while i < len(s):
+
+            index+=1
+
+            if s[i]=="{":
+                group=""
+
+                i+=1
+
+                while s[i] !="}":
+
+                    if s[i] !=',':
+                       group+=(s[i])
+                    i+=1
+
+                groups.append(group)
+
+            else:
+                groups[index].append(s[i])
+
+            i+=1
+
+
+        
+
+        
