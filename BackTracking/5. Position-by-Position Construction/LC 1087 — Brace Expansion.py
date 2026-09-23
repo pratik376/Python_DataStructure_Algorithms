8647fa51ec1d73f62ec1ab25d9889e54b93fdf6e
pratik376 <babariyapratik208@gmail.com>
2026-09-22 18:26:40 -0700

    Changed method name

--- BackTracking/5. Position-by-Position Construction/LC 1087 — Brace Expansion.py
+++ BackTracking/5. Position-by-Position Construction/LC 1087 — Brace Expansion.py
@@ -3,35 +3,44 @@ from typing import List
 
 class Solution:
     def expand(self, s: str) -> List[str]:
+        groups = []
+        res=[]
 
-        index=-1
-        groups=[]
-
-        i=0
+        i = 0
 
         while i < len(s):
 
-            index+=1
+            if s[i] == "{":
+                group = ""
+                i += 1
+                while s[i] != "}":
 
-            if s[i]=="{":
-                group=""
-
-                i+=1
-
-                while s[i] !="}":
-
-                    if s[i] !=',':
-                       group+=(s[i])
-                    i+=1
-
+                    if s[i] != ",":
+                        group += s[i]
+                    i += 1
                 groups.append(group)
-
             else:
-                groups[index].append(s[i])
+                groups.append(s[i])
+            i += 1
+        groups = ["".join(sorted(group)) for group in groups]
 
-            i+=1
 
+        def dfs(position, path):
 
-        
+            if len(path)==len(groups):
 
-        
+                res.append(path[:])
+                return
+
+            for ch in groups[position]:
+
+                path.append(ch)
+
+                dfs(position+1, path)
+                path.pop()
+
+        dfs(0,[])
+
+        return res
+
+    
