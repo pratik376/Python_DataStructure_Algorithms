c9e28dc5cc4df0ce2c539cb0293ba10c23bc5850
pratik376 <babariyapratik208@gmail.com>
2026-09-23 17:07:27 -0700

    Changed method name

--- BackTracking/6. Constraint-Based Sequence Construction/LC 22 — Generate Parentheses.py
+++ BackTracking/6. Constraint-Based Sequence Construction/LC 22 — Generate Parentheses.py
@@ -1,3 +1,52 @@
+# def dfs(state):
+
+#     if complete:
+#         save_answer()
+#         return
+
+#     for choice in choices:
+
+#         if not valid(choice, state):
+#             continue
+
+#         # choose
+#         make_choice(choice)
+
+#         # explore
+#         dfs(new_state)
+
+#         # undo
+#         undo_choice(choice)
+
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
+
+        res=[]
+
+
+        def dfs(open, close, path):
+
+            if len(path) == 2 * n:
+
+                res.append("".join(path))
+                return
+            
+            for c in "()":
+
+                if c=="(":
+                    open+=1
+                else:
+                    close+=1
+
+                if close>open:
+                    continue
+
+                path.append(c)
+                dfs(open,close,path)
+                path.pop()
+
+            dfs(0,0,[])
+            return res
+
+                
         
